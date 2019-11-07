8ea32deecd5af70fd921e197e74d95932add68f4
JinHyoukYork <jdd04026@gmail.com>
2019-11-07 14:26:09 +0900

    TEST: Directory direction change cause test error
    디렉토리 경로가 달라 테스트 에러가 많이 나기 때문에 이것을 변경함

--- server/test/uuid.test.js
+++ server/test/uuid.test.js
@@ -1,5 +1,5 @@
 const app = require('../src');
-const { makeUUID } = require('../bin/uuid.js');
+const { makeUUID } = require('../src/bin/uuid.js');
 
 describe('Express GET /', () => {
     it('should return uuid', (done) => {
