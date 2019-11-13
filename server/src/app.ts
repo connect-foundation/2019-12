af7f5a86bbd944fb145cbd1939b6d69fccdf9ee3
FullOfOrange <foorgdev@gmail.com>
2019-11-13 17:44:06 +0900

    feat: add cookie-parser dependency
    쿠키 파서에 대한 내용을 추가함.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,13 @@
 import * as express from 'express';
+import cookieParser = require('cookie-parser');
+
 // Router Imports
 import userRouter from './routes/v1/api/user';
 
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/users', userRouter);
 
