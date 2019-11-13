c669fbb258e6bbce145d47b470008db6655ea833
FullOfOrange <foorgdev@gmail.com>
2019-11-13 17:43:20 +0900

    feat: add router template
    라우터 템플릿을 추가함

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,10 @@
 import * as express from 'express';
+// Router Imports
+import userRouter from './routes/v1/api/user';
 
 const app = express();
+
+
+app.use('/api/users', userRouter);
 
 export default app;
--- server/src/routes/v1/api/user/index.ts
+++ server/src/routes/v1/api/user/index.ts
@@ -1,13 +1,15 @@
 import * as express from 'express';
-// types
-// import Request from '../../../../types/Request';
-// Controllers
+
 import createUser from './controller/createUser';
 
 const router = express.Router();
 
 // 전체를 관통할 미들웨어를 이곳에 서술
-router.use();
+router.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    next();
+  },
+);
 // parameter를 뽑을 때 사용할 미들웨어
 router.param(
   'userId',
@@ -26,7 +28,10 @@ router.param(
 router.get('/:userId/events');
 router
   .route('/:userId')
-  .get()
+  .get((req: express.Request, res: express.Response) => {
+    res.send('GET /users');
+  })
   .patch();
 router.post('/', createUser);
-export = router;
+
+export default router;
