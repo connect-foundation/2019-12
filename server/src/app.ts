3410d992dd2825683a8f0ac4f242f005060f2865
FullOfOrange <foorgdev@gmail.com>
2019-11-14 11:06:17 +0900

    feat: add passport auth router
    패스포트 인증에 사용할 라우터 추가

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as cookieParser from 'cookie-parser';
 
 // Router Imports
+import authRouter from './routes/api/auth';
 import userRouter from './routes/api/users';
 
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 
 export default app;
---
+++ server/src/routes/api/auth/index.ts
@@ -0,0 +1,8 @@
+import * as express from 'express';
+
+const router = express.Router();
+
+router.get('/callback');
+router.get('/')
+
+export default router;
