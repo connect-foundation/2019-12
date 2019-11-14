eeaed9bde962d899f0f43a24f2b0ac7087222321
doong-jo <sdong001@gmail.com>
2019-11-14 15:45:31 +0900

    feat: add CreateEventBtn Component (draft)
    CreateEventBtn 컴포넌트 초안 작성

---
+++ client/src/components/atoms/CreateEventBtn/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import * as S from './style';
+import { CREATE_EVENT } from '../../../commons/constants/string';
+
+const CreateEventBtn: React.FC = () => <S.Btn>{CREATE_EVENT}</S.Btn>;
+
+export default CreateEventBtn;
