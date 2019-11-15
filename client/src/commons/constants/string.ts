e1b88a8027a2fe3152e106b56b45b40f725841fa
inthewalter <21400564@handong.edu>
2019-11-15 17:58:36 +0900

    chore: add constant values for UI components
     - UI Component에 들어갈 상수값들을 추가
    - 추후 변화를 쉽게 하기 위함

--- client/src/commons/constants/string.ts
+++ client/src/commons/constants/string.ts
@@ -1,3 +1,8 @@
 export const CREATE_EVENT = '이벤트 주최하기';
 export const LOGO = 'BookUs!';
 export const BEFORE_LOGIN = '가입 혹은 로그인';
+export const EMAIL = '이메일 주소';
+export const LAST_NAME = '성';
+export const FIRST_NAME = '이름';
+export const PHONE = '휴대폰 번호';
+export const SIGN_UP_SUBMIT = '회원가입';
