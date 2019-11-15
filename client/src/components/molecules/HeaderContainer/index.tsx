a423ae3dc9c23c1402b6ce17566572e8e648a86b
inthewalter <21400564@handong.edu>
2019-11-15 18:01:50 +0900

    feat: add HeaderContainer Component
      - atoms를 조합한 molecules level
      - 이것 역시 아직 로직과 스타일은 고려X

--- client/src/components/molecules/HeaderContainer/index.tsx
+++ client/src/components/molecules/HeaderContainer/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import * as S from './style';
 
-import CreateEventBtn from '../../atoms/CreateEventBtn';
 import LogoBtn from '../../atoms/LogoBtn';
-import AccountBtn from '../../atoms/AccountBtn';
-import { BEFORE_LOGIN } from '../../../commons/constants/string';
+import { BEFORE_LOGIN, CREATE_EVENT } from '../../../commons/constants/string';
 
 const HeaderContainer: React.FC = () => (
   <S.Container>
-    <CreateEventBtn />
+    <S.CreateEventBtn content={CREATE_EVENT} />
     <LogoBtn />
-    <AccountBtn content={BEFORE_LOGIN} />
+    <S.AccountBtn content={BEFORE_LOGIN} />
   </S.Container>
 );
 
--- client/src/components/molecules/HeaderContainer/style.ts
+++ client/src/components/molecules/HeaderContainer/style.ts
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
+import Btn from '../../atoms/Btn';
+
 export const Container = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
+export const CreateEventBtn = styled(Btn)``;
+export const AccountBtn = styled(Btn)``;
