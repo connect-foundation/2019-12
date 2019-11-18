d19aeed226320a4ca73e187779cc7946d788d2f5
doong-jo <sdong001@gmail.com>
2019-11-18 09:30:45 +0900

    refactor: modify duplicated white palette
    리뷰를 반영하여 중복되는 white palette를 수정

--- client/src/commons/style/themes/default.ts
+++ client/src/commons/style/themes/default.ts
@@ -5,7 +5,7 @@ const theme = {
     danger: ['#d32f2f', '#f44336', '#f8877f', '#ffcdd2'],
     alert: ['#ffa000', '#ffc107', '#ffd761', '#ffecb3'],
     success: ['#388e3c', '#4caf50', '#7cc47f', '#c8e6c9'],
-    white: ['#fff', '#fff', '#eee'],
+    white: ['#fff', '#eee', '#ddd'],
     grayscale: [
       '#212121',
       '#414141',
