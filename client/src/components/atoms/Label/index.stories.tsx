2fd3df0a81856b7c78c58d962238bbfdcffcff9b
inthewalter <21400564@handong.edu>
2019-11-19 14:46:17 +0900

    feat: edit Label Component
    - 컨벤션에 맞게 수정
    - 라벨 뒤 * 를 pseudo element를 활용해서 렌더링

--- client/src/components/atoms/Label/index.stories.tsx
+++ client/src/components/atoms/Label/index.stories.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
 import { text, boolean } from '@storybook/addon-knobs';
 import Label from '.';
 
-storiesOf('0 - Atoms - Label', module)
-  .add('default', () => (
-    <Label
-      required={boolean('required', false)}
-      name={text('name', '이메일')}
-    />
-  ))
-  .add('required', () => (
-    <Label required={boolean('required', true)} name={text('name', '이메일')} />
-  ));
+export default {
+  title: 'Atoms / Label',
+};
+
+export const defaultLabel: React.FC = () => (
+  <Label required={boolean('required', false)} name={text('name', '이메일')} />
+);
+
+export const requiredLabel: React.FC = () => (
+  <Label required={boolean('required', true)} name={text('name', '이메일')} />
+);
