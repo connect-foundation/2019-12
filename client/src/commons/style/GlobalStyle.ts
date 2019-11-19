35eabd4a7719dcca41526038f31b595f31107ce8
doong-jo <sdong001@gmail.com>
2019-11-19 13:42:43 +0900

    feat: add globalstyle spec
    - a tag에서 파란색 밑줄로 표시되는 decoriation 제거
    - html에서 font-size를 10px로 설정하여 1rem == 10px로 명시

--- client/src/commons/style/GlobalStyle.ts
+++ client/src/commons/style/GlobalStyle.ts
@@ -5,19 +5,19 @@ const GlobalStyles = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css?family=Montserrat|Roboto');
     @import url('https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap');
     margin: 0;
-    font-family: 'Roboto', 'Noto Sans KR', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
+    font-family: 'Noto Sans KR', 'Roboto', sans-serif;
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
-  html,
+  html {
+    font-size: 10px;
+  }
   body {
     height: 100%;
   }
-  code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+  a {
+    text-decoration: none;
   }
 `;
 
