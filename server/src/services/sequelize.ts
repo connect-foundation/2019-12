08347d25ddd4cd18e6ed02559d839279c140709a
FullOfOrange <foorgdev@gmail.com>
2019-11-19 18:21:08 +0900

    fix: travis root not use password

--- server/src/services/sequelize.ts
+++ server/src/services/sequelize.ts
@@ -13,7 +13,7 @@ const { DB_HOST, DB_USER, DB_PW, DB_NAME } = process.env;
 export const sequelize = new Sequelize({
   host: DB_HOST,
   username: DB_USER,
-  password: DB_PW || undefined,
+  password: DB_PW || '',
   database: DB_NAME,
   dialect: 'mariadb',
   models: [Event, Order, OrderTicket, TicketSubscription, TicketType, User],
