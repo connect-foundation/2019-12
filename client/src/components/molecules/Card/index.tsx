000141f48503412ec9cd4de749fd3450bf6bad79
doong-jo <sdong001@gmail.com>
2019-11-19 14:31:31 +0900

    feat: add Card component
    card component를 작성하고 사용되는 상수도 함께 추가함

---
+++ client/src/components/molecules/Card/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { EVENT_NAME_MAX_LENGTH } from '../../../commons/constants/number';
+import * as S from './style';
+import Divider from '../../atoms/Divider';
+
+interface Props {
+  /** 이벤트 URL */
+  href: string;
+  /** 이벤트 이름 */
+  imgSrc: string;
+  /** 이벤트 날짜 */
+  date: string;
+  /** 이벤트 이름 */
+  name: string;
+  /** 호스트 이름 */
+  host: string;
+  /** 가격 */
+  price: string;
+}
+
+function Card({
+  href,
+  imgSrc,
+  date,
+  name,
+  host,
+  price,
+}: Props): React.ReactElement {
+  return (
+    <S.LinkWrapper href={href}>
+      <S.HeaderWrapper></S.HeaderWrapper>
+      <S.ImgDiv imgSrc={imgSrc} />
+      <S.InnerContainer>
+        <S.ContentContainer>
+          <S.Date>{date}</S.Date>
+          <S.Name>{`${name.slice(0, EVENT_NAME_MAX_LENGTH)}...`}</S.Name>
+          <S.Host>{host}</S.Host>
+        </S.ContentContainer>
+        <Divider />
+        <S.Price>{price}</S.Price>
+      </S.InnerContainer>
+    </S.LinkWrapper>
+  );
+}
+
+export default Card;
