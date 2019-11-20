346178fc42fa8668e43326ecd4921b68ee5adac6
inthewalter <21400564@handong.edu>
2019-11-20 18:49:12 +0900

    feat: add draft FormInput Component
    - FormInput 초안 작성
    - 라벨의 유무를 Props로 받아야할 지?

---
+++ client/src/components/molecules/FormInput/style.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import { ifProp, palette, theme } from 'styled-tools';
+import Input from '../../atoms/Input';
+
+interface Props {
+  invalid?: boolean;
+}
+
+export const FormInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const FormInput = styled(Input)`
+  margin: 0.6rem 0;
+`;
+export const FormCaption = styled.div<Props>`
+  ${theme('fontStyle.caption')}
+  visibility: ${ifProp('invalid', 'unset', 'hidden')};
+  color: ${palette('primary')};
+`;
