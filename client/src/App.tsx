3202f1a17274135d9336965637bba9787b0b9883
FullOfOrange <foorgdev@gmail.com>
2019-11-20 20:41:38 +0900

    feat: add router to Login page
    라우터에 로그인 페이지를 접근할 수 있는 라우팅을 추가함

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+import Login from './pages/Login';
 
 const App: React.FC = () => (
   <Router>
     <div className="App">
-      <Switch >
+      <Link to="/login">Login</Link>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
         <Route path="*">
           <div>404 page</div>
         </Route>
