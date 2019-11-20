f068812b77f14af17f2d293799291ad3b90dbad6
doong-jo <sdong001@gmail.com>
2019-11-20 17:46:49 +0900

    refactor: modify footer (resize, divider)
    - figma를 참고하여 더 작게 축소
    - Divider를 추가

--- client/src/components/organisms/Footer/index.tsx
+++ client/src/components/organisms/Footer/index.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 import * as S from './style';
 import logoGray from '../../../assets/img/logo-gray.svg';
 import { FOOTER_INFO } from '../../../commons/constants/string';
-
+import Divider from '../../atoms/Divider';
 
 function Footer(): React.ReactElement {
   return (
     <>
-      <S.Img alt={'Logo'} src={logoGray} />
-      <S.Info>{FOOTER_INFO}</S.Info>
+      <Divider />
+      <S.Container>
+        <S.Img alt={'Logo'} src={logoGray} />
+        <S.Info>{FOOTER_INFO}</S.Info>
+      </S.Container>
     </>
   );
 }
--- client/src/components/organisms/Footer/style.ts
+++ client/src/components/organisms/Footer/style.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 import { palette, theme } from 'styled-tools';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding: 2rem 0rem;
+`;
 
 export const Img = styled.img`
-  height: 4.5rem;
+  height: 2.5rem;
 `;
 
 export const Info = styled.div`
