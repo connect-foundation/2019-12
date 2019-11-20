6f1cb8c8fce639fef4c010ff36748fd2daa68b45
문종현 <dobest27@gmail.com>
2019-11-20 15:19:38 +0900

    refactor: db seed
    testable 한 코드를 위한 리팩토링
    bin/seed 에 있던 코드를 services/sequelize 로 옮기고,
    그것을 실행하는 코드만 남김

--- server/bin/seed.ts
+++ server/bin/seed.ts
@@ -1,28 +1,5 @@
 import '../src/env';
-import { sequelize } from '../src/services/sequelize';
-import { Event, TicketType } from '../src/models';
-import { readFileSync } from 'fs';
+import { sequelize, seed } from '../src/services/sequelize';
 
 sequelize.options.logging = false;
 seed();
-
-export async function seed() {
-  try {
-    console.info('DB seed start...');
-
-    console.info('creating event data..');
-    const eventData = readFileSync('./db/events.json').toString();
-    const eventsBulk = JSON.parse(eventData);
-    await Event.bulkCreate(eventsBulk);
-
-    console.info('creating tickettype data..');
-    const ticketTypeData = readFileSync('./db/ticketTypes.json').toString();
-    const ticketTypeBulk = JSON.parse(ticketTypeData);
-    await TicketType.bulkCreate(ticketTypeBulk);
-
-    console.info('DB seed end...');
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-}
--- server/src/services/sequelize.ts
+++ server/src/services/sequelize.ts
@@ -7,6 +7,7 @@ import {
   TicketType,
   User,
 } from '../models';
+import { readFileSync } from 'fs';
 
 const { DB_HOST, DB_USER, DB_PW, DB_NAME } = process.env;
 
@@ -24,6 +25,27 @@ export async function migrate() {
     console.info('DB migration start...');
     await sequelize.sync();
     console.info('DB migration end...');
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+export async function seed() {
+  try {
+    console.info('DB seed start...');
+
+    console.info('creating event data..');
+    const eventData = readFileSync('../db/events.json').toString();
+    const eventsBulk = JSON.parse(eventData);
+    await Event.bulkCreate(eventsBulk);
+
+    console.info('creating tickettype data..');
+    const ticketTypeData = readFileSync('../db/ticketTypes.json').toString();
+    const ticketTypeBulk = JSON.parse(ticketTypeData);
+    await TicketType.bulkCreate(ticketTypeBulk);
+
+    console.info('DB seed end...');
   } catch (error) {
     console.error(error);
     process.exit(1);
