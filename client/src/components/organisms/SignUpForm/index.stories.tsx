a05a1302d534fcc3d3c97795b7e1cca738480c99
inthewalter <21400564@handong.edu>
2019-11-21 19:26:52 +0900

    feat: edit SignUpForm Storybook file
    - 스토리북에서 Hooks를 사용하는 데 버그가 있어서 Knob으로 대체

--- client/src/components/organisms/SignUpForm/index.stories.tsx
+++ client/src/components/organisms/SignUpForm/index.stories.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
+
 import SignUpForm from '.';
+import { text } from '@storybook/addon-knobs';
 
 export default {
   title: 'Organisms / SignUpForm',
 };
 
 export const defaultInput: React.FC = () => {
-  const [state, setState] = React.useState({
-    email: '',
-    lastName: '',
-    firstName: '',
-    phoneNumber: '',
-    password: '',
-  });
-  const updateField = (e: any) => {
-    setState({
-      ...state,
-      [e.target.name]: e.target.value,
-    });
+  const state = {
+    email: text('email', ''),
+    lastName: text('lastName', ''),
+    firstName: text('firstName', ''),
+    phoneNumber: text('phoneNumber', ''),
+    password: text('password', ''),
   };
-  return <SignUpForm state={state} setState={updateField} />;
+  return (
+    <>
+      <h1 style={{ marginBottom: '4rem' }}>
+        *state를 주입해야 하는 컴포넌트이기 때문에 아래의 Knob로 인풋 변경 가능
+      </h1>
+      <SignUpForm state={state} setState={() => {}} />
+    </>
+  );
 };
