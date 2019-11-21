33cb60875612772ecfa8ba07536c436f490dabc3
FullOfOrange <foorgdev@gmail.com>
2019-11-22 00:39:43 +0900

    test: make test code about seed
    시드 데이터를 올리고 그것을 테스트 하는 코드를 만듦

--- server/test/db/seed.spec.ts
+++ server/test/db/seed.spec.ts
@@ -1,63 +1,66 @@
 import '../../src/env';
-import { sequelize, migrate, seed } from '../../src/services/sequelize';
-import { User, Event } from '../../src/models';
-import { readJSONData } from '../../src/utils/readJSON';
-import { resolve } from 'path';
+import { sequelize } from '../../src/services/sequelize';
+import {
+  User,
+  Event,
+  TicketType,
+  Order,
+  OrderTicket,
+  TicketSubscription,
+} from '../../src/models';
 
-const SEED_DIR = resolve(__dirname, '../../db_seed');
-
-beforeAll(async () => {
-  sequelize.options.logging = false;
-  try {
-    await migrate();
-    await seed();
-  } catch (error) {
-    // UniqueConstraintError
-    console.info('already seed');
-  }
-});
+const exclude = ['createdAt', 'updatedAt'];
 
 afterAll(async () => {
   await sequelize.close();
 });
-
-const isSame = <T>(target: T, origin: T): boolean => {
-  const keys = Object.keys(origin) as (keyof T)[];
-  return keys.every(key => {
-    if (!origin[key]) return true;
-
-    if (typeof target[key] === 'object' && typeof origin[key] === 'string')
-      expect(target[key]).toMatchObject(new Date(JSON.stringify(origin[key])));
-    else expect(target[key]).toBe(origin[key]);
-    return true;
-  });
-};
-
 describe('DB seed 데이터가 유효', () => {
   it('User 데이터가 유효', async () => {
-    // given
-    const users = await User.findAll();
-    const usersPath = resolve(SEED_DIR, 'users.json');
-    const origins = await readJSONData<User>(usersPath);
-
-    // when
-    users.forEach(user => {
-      const origin = origins.find(origin => origin.id === user.id);
-
-      // then
-      isSame(user, origin);
+    //given, when
+    const users = await User.findAll({
+      attributes: { exclude: exclude },
     });
+    //then
+    expect(users).toMatchSnapshot();
   });
-
-  it('Event 데이터가 유효', async done => {
-    const events = await Event.findAll();
-    const eventPath = resolve(SEED_DIR, 'events.json');
-    const origins = await readJSONData<Event>(eventPath);
-
-    events.forEach(event => {
-      const origin = origins.find(origin => origin.id === event.id);
-      isSame(event, origin);
+  it('Event 데이터가 유효', async () => {
+    //given, when
+    const events = await Event.findAll({
+      attributes: { exclude: exclude },
     });
-    done();
+    //then
+    expect(events).toMatchSnapshot();
+  });
+  it('TicketType 데이터가 유효', async () => {
+    //given, when
+    const tickettypes = await TicketType.findAll({
+      attributes: { exclude: exclude },
+    });
+    //then
+    expect(tickettypes).toMatchSnapshot();
+  });
+  it('Order 데이터가 유효', async () => {
+    //given, when
+    const orders = await Order.findAll({
+      attributes: { exclude: exclude },
+    });
+    //then
+    expect(orders).toMatchSnapshot();
+  });
+  it('OrderTicket 데이터가 유효', async () => {
+    //given, when
+    const ordertickets = await OrderTicket.findAll({
+      attributes: { exclude: exclude },
+    });
+    //then
+    expect(ordertickets).toMatchSnapshot();
+  });
+  it('TicketSubscription 데이터가 유효', async () => {
+    //given, when
+    const ticketsubscription = await TicketSubscription.findAll({
+      attributes: { exclude: exclude },
+    });
+    //then
+    expect(ticketsubscription).toMatchSnapshot();
   });
 });
