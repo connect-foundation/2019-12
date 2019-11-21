79d35b9e9c06649afe883ec131a5d52207a15124
문종현 <dobest27@gmail.com>
2019-11-21 21:01:13 +0900

    feat: add try-catch expression to seed bin
    에러 처리를 위해 seed 를 try-catch expression 으로 래핑

--- server/bin/seed.ts
+++ server/bin/seed.ts
@@ -2,4 +2,12 @@ import '../src/env';
 import { sequelize, seed } from '../src/services/sequelize';
 
 sequelize.options.logging = false;
-seed();
+
+(async () => {
+  try {
+    await seed();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+})();
