55677f4e96d6ee57cbe7c7ae69fb7b569ed89abb
inthewalter <21400564@handong.edu>
2019-11-21 10:59:41 +0900

    chore: remove unused tags in App Component
    - 필요없는 태그와 CSS를 삭제

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
-import './App.css';
 import defaultTheme from './commons/style/themes/default';
 import Normalize from './commons/style/Normalize';
 import GlobalStyles from './commons/style/GlobalStyle';
@@ -12,13 +11,11 @@ const App: React.FC = () => (
     <Normalize />
     <GlobalStyles />
     <Router>
-      <div className="App">
-        <Switch>
-          <Route path="*">
-            <div>404 page</div>
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="*">
+          <div>404 page</div>
+        </Route>
+      </Switch>
     </Router>
   </ThemeProvider>
 );
