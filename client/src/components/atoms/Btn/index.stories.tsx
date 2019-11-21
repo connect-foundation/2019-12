58f5aa78a22a56f159594039e678957b2c5a3b60
inthewalter <21400564@handong.edu>
2019-11-21 19:26:08 +0900

    chore: remove unused module in Btn Component
    - 버튼 컴포넌트에서 더이상 라우팅관련 코드를 쓰지 않기 때문에 삭제

--- client/src/components/atoms/Btn/index.stories.tsx
+++ client/src/components/atoms/Btn/index.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { text, boolean, select } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Btn from '.';
 
