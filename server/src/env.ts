d8e019d2fc8b5670f68fd952347f5e777cbeb556
문종현 <dobest27@gmail.com>
2019-11-21 16:30:17 +0900

    refactor: dotenv config
    dot env 파일을 node_env에 맞게 불러올 수 있도록 리팩토링

--- server/src/env.ts
+++ server/src/env.ts
@@ -1,2 +1,8 @@
 import { config } from 'dotenv';
-config();
+import { resolve } from 'path';
+
+const NODE_ENV = process.env.NODE_ENV;
+const envFileName = NODE_ENV ? `.env.${NODE_ENV}` : '.env';
+const path = resolve(__dirname, `../${envFileName}`);
+
+config({ path });
