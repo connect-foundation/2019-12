3a56a5541d3f0820fbbd18fe32bce0846c736398
Yongho Lee <21400564@handong.edu>
2019-11-25 16:36:59 +0900

    feat: add CardGrid Component
    - 그리드 형식으로 구현
    - Mock 데이터로 구현

---
+++ client/src/components/organisms/CardGrid/index.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import CardGrid from '.';
+
+export default {
+  title: 'Organisms / CardGrid',
+};
+
+export const cardGrid: React.FC = () => {
+  return <CardGrid />;
+};
