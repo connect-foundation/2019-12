db0379e372b03b971e98e4cf1d2d1df0fd828b58
문종현 <dobest27@gmail.com>
2019-11-25 19:08:53 +0900

    feat: add events route to index route
    event 라우트 등록

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import authRouter from './auth';
 import userRouter from './users';
+import eventRouter from './events';
 
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/users', userRouter);
+router.use('/events', eventRouter);
 
 export default router;
