38ce459ac5721356cfeefd8700e6e7212807e1da
문종현 <dobest27@gmail.com>
2019-11-26 22:07:04 +0900

    feat: let www script use port in env variables
    www 에서 port 환경변수를 읽어 실행하도록 변경하고,
    port 환경변수가 없다면 exit 하도록 함

--- server/bin/www.ts
+++ server/bin/www.ts
@@ -2,5 +2,12 @@ import '../src/env';
 import '../src/utils/sequelize';
 import app from '../src/app';
 
-app.listen(3000);
+const { PORT } = process.env;
+
+if (!PORT) {
+  console.error('환경변수 파일에 PORT 가 필요합니다.');
+  process.exit(1);
+}
+
+app.listen(PORT);
 console.info('Server is running...!!');
