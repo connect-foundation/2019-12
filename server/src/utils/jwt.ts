53c28c184b16997b12b1d187025a45ce2797918e
FullOfOrange <foorgdev@gmail.com>
2019-11-26 19:00:49 +0900

    feat: make generating JWT function
    Token을 발급하는 함수를 제작

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -1,18 +1,21 @@
 import * as JWT from 'jsonwebtoken';
+const { JWT_SECURE } = process.env;
 
 export function generateJWT(
+  exist: boolean,
   id: number,
   googleId: number,
-  email?: string,
+  email: string,
 ): Promise<JWT.Secret> {
   return new Promise((resolve, reject) => {
     JWT.sign(
       {
+        exist,
         id,
         googleId,
         email,
       },
-      'secret',
+      JWT_SECURE || '',
       {
         // Expire Date를 외부로 빼야함.
         expiresIn: 1000 * 60 * 60 * 24,
