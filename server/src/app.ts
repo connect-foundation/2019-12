9d4c34b6b97bb80a27cc6313981002a4043c5c5c
FullOfOrange <foorgdev@gmail.com>
2019-11-26 19:03:51 +0900

    feat: add cors module in app.ts to setting cookie

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,21 @@
 import * as express from 'express';
 import * as cookieParser from 'cookie-parser';
 import * as passport from 'passport';
-
+import * as cors from 'cors';
 import setUpPassport from '../src/services/passport';
 import indexRouter from './routes/api';
 
+const { CLIENT_URL } = process.env;
+
 const app = express();
 
+app.use(
+  cors({
+    origin: CLIENT_URL,
+    credentials: true,
+    exposedHeaders: ['Uid', 'Access-Token'],
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(passport.initialize());
