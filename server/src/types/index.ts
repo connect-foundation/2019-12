75343e487d8ed6724326acf45f61a36cbc8b7e5e
FullOfOrange <foorgdev@gmail.com>
2019-11-27 19:55:38 +0900

    feat: make token type

---
+++ server/src/types/index.ts
@@ -0,0 +1,1 @@
+export { tokentype } from './token';
---
+++ server/src/types/token.ts
@@ -0,0 +1,8 @@
+export type tokentype = {
+  exist: boolean;
+  id: number;
+  googleId: number;
+  email: string;
+  iat?: number;
+  exp?: number;
+};
