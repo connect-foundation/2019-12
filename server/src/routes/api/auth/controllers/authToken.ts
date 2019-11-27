037b157d8327b76032a34bd529e34945861d8b71
FullOfOrange <foorgdev@gmail.com>
2019-11-27 19:58:50 +0900

    feat: make verify token api
    토큰을 검증하는 api를 만듦

--- server/src/routes/api/auth/controllers/authToken.ts
+++ server/src/routes/api/auth/controllers/authToken.ts
@@ -1,13 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
 import * as JWT from 'jsonwebtoken';
+
 const { JWT_SECURE } = process.env;
 
 export const authToken = (req: Request, res: Response, next: NextFunction) => {
   const token = req.cookies.UID;
-  console.log(token);
-  JWT.verify(token, JWT_SECURE || '', (err: any, decoded: any) => {
-    console.log(decoded);
-    res.send(decoded);
+  JWT.verify(token, JWT_SECURE || '', (err: JWT.VerifyErrors, decoded: any) => {
+    if (err) {
+      next(err);
+    }
+    const decodeToken = {
+      exist: decoded.exist,
+      id: decoded.id,
+      googleId: decoded.googleId,
+      email: decoded.email,
+    };
+    res.send(decodeToken);
   });
-  res.send();
 };
