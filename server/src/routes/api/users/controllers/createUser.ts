e33cd3ee135f5faa3424c8a9f97d653cb20ae424
FullOfOrange <foorgdev@gmail.com>
2019-11-27 19:57:23 +0900

    feat: make POST /api/users api
    유저를 생성하는 api를 만듦

--- server/src/routes/api/users/controllers/createUser.ts
+++ server/src/routes/api/users/controllers/createUser.ts
@@ -1,7 +1,22 @@
-import * as express from 'express';
+import { Request, Response } from 'express';
+import { setUserInfo } from '../../../../services';
 
-function createUser(req: express.Request, res: express.Response) {
-  res.send();
+export async function createUser(req: Request, res: Response) {
+  const { exist, id, googleId, firstName, lastName, phoneNumber } = req.body;
+  if (!exist) {
+    const result = await setUserInfo(
+      id,
+      googleId,
+      firstName,
+      lastName,
+      phoneNumber,
+    );
+    if (result[0] === 1) {
+      res.send({ message: 'Success Signup' });
+    } else {
+      res.status(400).send({ message: 'Cannot Signup' });
+    }
+  } else {
+    res.status(400).send({ message: 'Cannot Signup' });
+  }
 }
-
-export default createUser;
