71a1429286a3fb8b9bf5f15000b455f18ab42eb5
doong-jo <sdong001@gmail.com>
2019-11-28 17:30:37 +0900

    refactor: add IconBtn with circular image
    IconBtn에 circular img가 추가 가능하게 수정함
    기존에 IconBtn을 사용하던 Login도 함께 수정함(props)

--- client/src/components/molecules/IconBtn/index.tsx
+++ client/src/components/molecules/IconBtn/index.tsx
@@ -3,14 +3,18 @@ import React from 'react';
 import * as S from './style';
 
 interface Props {
-  /** 접근성 대응 */
-  alt: string;
   /** 아이콘 소스 */
-  src: any;
+  iconSrc: any;
+  /** hover 아이콘 소스 */
+  hoveredIconSrc?: any;
+  /** 이미지 소스 */
+  circleImgSrc?: any;
   /** 버튼 내용 */
   content: string;
   /** 크기 */
   height?: string;
+  /** 아이콘 크기 */
+  IconHeight?: string;
   /** button styling type */
   styletype?: string;
   /** click handler */
@@ -20,17 +24,39 @@ interface Props {
 }
 
 function IconBtn({
-  content,
-  onClick,
   styletype = 'primary',
   fullid = false,
+  IconHeight = '1.5rem',
   ...props
 }: Props): React.ReactElement {
+  const { circleImgSrc } = props;
   return (
-    <S.Container fullid={fullid} onClick={onClick} styletype={styletype}>
+    <S.Container
+      fullid={fullid}
+      onClick={props.onClick}
+      styletype={styletype}
+      hoveredIconSrc={props.hoveredIconSrc}
+    >
       <S.Wrapper>
-        <S.IconInBtn {...props} />
-        <S.Content>{content}</S.Content>
+        {circleImgSrc && (
+          <>
+            <S.CircleIconImg
+              alt={'icon'}
+              circular={true}
+              src={circleImgSrc}
+              height={`${parseInt(IconHeight) + 2}rem`}
+            />
+          </>
+        )}
+        <S.Content>{props.content}</S.Content>
+        <S.IconImg alt={'icon'} src={props.iconSrc} height={IconHeight} />
+        {props.hoveredIconSrc && (
+          <S.HoveredIconImg
+            alt={'icon'}
+            src={props.hoveredIconSrc}
+            height={IconHeight}
+          />
+        )}
       </S.Wrapper>
     </S.Container>
   );
