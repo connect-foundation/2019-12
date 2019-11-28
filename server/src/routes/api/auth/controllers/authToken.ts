df6cae0eabb04eebdfa6a3e878d8511dfa3f574d
FullOfOrange <foorgdev@gmail.com>
2019-11-28 16:51:23 +0900

    feat: checking req.body

--- server/src/routes/api/auth/controllers/authToken.ts
+++ server/src/routes/api/auth/controllers/authToken.ts
@@ -6,15 +6,19 @@ const { JWT_SECURE } = process.env;
 export const authToken = (req: Request, res: Response, next: NextFunction) => {
   const token = req.cookies.UID;
   JWT.verify(token, JWT_SECURE || '', (err: JWT.VerifyErrors, decoded: any) => {
-    if (err) {
-      next(err);
+    if (err) next(err);
+    const value = Object.keys(decoded).every(key => key !== undefined);
+    if (value) {
+      const { exist, id, googleId, email } = decoded;
+      const decodeToken = {
+        exist,
+        id,
+        googleId,
+        email,
+      };
+      res.send(decodeToken);
+    } else {
+      next({ message: 'unable to verify token' });
     }
-    const decodeToken = {
-      exist: decoded.exist,
-      id: decoded.id,
-      googleId: decoded.googleId,
-      email: decoded.email,
-    };
-    res.send(decodeToken);
   });
 };
