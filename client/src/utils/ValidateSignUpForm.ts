b7f328ae1718fb126e88085b2fa4477467cb22be
FullOfOrange <foorgdev@gmail.com>
2019-11-28 16:40:06 +0900

    refactor: validation form util & make test code

--- client/src/utils/ValidateSignUpForm.ts
+++ client/src/utils/ValidateSignUpForm.ts
@@ -1,20 +1,8 @@
-export function validatePhoneNumber(
-  number: string,
-  type = false,
-): boolean {
-  const phonereg = /010[\d]{7,8}/;
+export function validatePhoneNumber(number: string, type = false): boolean {
   if (number.length === 0) return !type;
-  if (number.length > 11) return false;
-  if (!number.match(phonereg)) return false;
-  return true;
+  return /^010[\d]{8}$/.test(number);
 }
 export function validateName(name: string, type = false): boolean {
-  const namereg = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]{0,5}/;
   if (name.length === 0) return !type;
-  if (name.length > 5) return false;
-
-  const regex = name.match(namereg);
-  if (regex === null) return false;
-  if (regex[0] !== regex.input) return false;
-  return true;
+  return /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]{0,5}$/.test(name);
 }
