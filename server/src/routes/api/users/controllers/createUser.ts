2ffbcd3c9562efb9bf1e9f84ff1f655744428981
FullOfOrange <foorgdev@gmail.com>
2019-11-28 14:02:36 +0900

    chore: add comments

--- server/src/routes/api/users/controllers/createUser.ts
+++ server/src/routes/api/users/controllers/createUser.ts
@@ -3,6 +3,7 @@ import { setUserInfo } from '../../../../services';
 
 export async function createUser(req: Request, res: Response) {
   const { exist, id, googleId, firstName, lastName, phoneNumber } = req.body;
+  // Exist가 False일 경우, Token이 회원가입이 안되어 있음을 말함.
   if (!exist) {
     const result = await setUserInfo(
       id,
