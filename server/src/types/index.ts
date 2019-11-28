48ae765e637c1fb45b6823333646b63b249291e7
FullOfOrange <foorgdev@gmail.com>
2019-11-28 16:33:51 +0900

    feat: erase token type

--- server/src/types/index.ts
+++
@@ -1,1 +0,0 @@
-export { tokentype } from './token';
--- server/src/types/token.ts
+++
@@ -1,8 +0,0 @@
-export type tokentype = {
-  exist: boolean;
-  id: number;
-  googleId: number;
-  email: string;
-  iat?: number;
-  exp?: number;
-};
