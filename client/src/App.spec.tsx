d986f098c9ae2972f2225369f490f24bce753b1b
Yongho Lee <21400564@handong.edu>
2019-11-29 09:11:13 +0900

    feat: add polyfills for intersectionObserver
    - jest를 돌릴 때 intersectionObserver를 찾지 못함
    - 따라서 intersectionObserver에 대한 polyfill 추가

--- client/src/App.spec.tsx
+++ client/src/App.spec.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'intersection-observer';
 
 describe('App', () => {
   it('renders without crashing', () => {
