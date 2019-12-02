333524178ad8fb2d5f21c7ab7bbdb389e6f36f1a
doong-jo <sdong001@gmail.com>
2019-12-02 13:00:35 +0900

    feat: add Location interface in Data types

--- client/src/types/Data.ts
+++ client/src/types/Data.ts
@@ -2,11 +2,10 @@ export interface User {
   id: number;
   lastName: string;
   firstName: string;
+  profileImgUrl: string;
 }
 
 export interface TicketType {
-  id: number;
-  eventId: number;
   name: string;
   desc: string;
   price: number;
@@ -30,4 +29,10 @@ export interface EventDetail {
   desc: string;
   ticketTypes: TicketType[];
   user: User;
+  location: Location;
+}
+
+export interface Location {
+  lat: number;
+  lng: number;
 }
