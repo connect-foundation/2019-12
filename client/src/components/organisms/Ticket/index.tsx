a1af5c42d14170f77ccaadb610e65bb613cd58c1
doong-jo <sdong001@gmail.com>
2019-12-02 12:56:09 +0900

    feat: add Ticket Component

---
+++ client/src/components/organisms/Ticket/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import * as S from './style';
+import { TicketType } from '../../../types/Data';
+import IconLabel from '../../molecules/IconLabel';
+
+import MultipleUsers from '../../../assets/img/multiple-users-silhouette.svg';
+import Check from '../../../assets/img/check-black.svg';
+import Calendar from '../../../assets/img/calendar-black.svg';
+import Price from '../../atoms/Price';
+
+function Ticket({
+  price,
+  name,
+  desc,
+  maxCntPerPerson,
+  salesEndAt,
+  leftCnt,
+}: TicketType): React.ReactElement {
+  return (
+    <>
+      <S.TicketLabel>티켓</S.TicketLabel>
+      <S.TicketContentContainer>
+        <S.TicketContentWrapContainer>
+          <S.TicketPriceWrapper>
+            <Price mount={price} separated={true} />
+          </S.TicketPriceWrapper>
+          <S.TicketName>{name}</S.TicketName>
+          <S.TicketDesc>{desc}</S.TicketDesc>
+          <IconLabel
+            iconProps={{ height: '1.5rem', alt: 'check', src: Check }}
+            labelStr={`1인당 ${maxCntPerPerson}개 구입 가능`}
+          />
+          <IconLabel
+            iconProps={{ height: '1.5rem', alt: 'calendar', src: Calendar }}
+            labelStr={`${salesEndAt.split('T')[0]} 판매마감`}
+          />
+          <IconLabel
+            iconProps={{
+              height: '1.5rem',
+              alt: 'people',
+              src: MultipleUsers,
+            }}
+            labelStr={`${leftCnt}개 판매`}
+          />
+        </S.TicketContentWrapContainer>
+      </S.TicketContentContainer>
+    </>
+  );
+}
+
+export default Ticket;
