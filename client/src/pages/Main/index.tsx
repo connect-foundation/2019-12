f4c98a668f58f8d9ffec2d4a9887ffcab7d009a8
Yongho Lee <21400564@handong.edu>
2019-12-02 19:22:54 +0900

    feat: add delay util function
    WHY
    - 딜레이 함수같은 경우는 일반적으로 쓸 수 있기 때문에 유틸로 분리하려고함
    
    WHAT
    - miliSeconds 만큼 멈추는 Promise기반의 함수 제작

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -6,17 +6,11 @@ import MainBanner from 'components/organisms/MainBanner';
 import CardGrid from 'components/organisms/CardGrid';
 import { Event } from '../../types/Event';
 import { useIntersect } from '../../hooks';
+import delay from '../../utils/delay';
 
 function Main(): React.ReactElement {
   const [events, setEvents] = useState<Event[]>([]);
   const { REACT_APP_SERVER_URL: SERVER_URL } = process.env;
-
-  const delay = (seconds: number): Promise<void> =>
-    new Promise(resolve =>
-      setTimeout(() => {
-        resolve();
-      }, seconds * 1000),
-    );
 
   const fetchItems = useCallback(async () => {
     const startAt =
@@ -28,7 +22,7 @@ function Main(): React.ReactElement {
       url: `${SERVER_URL}/api/events?cnt=12${startAt}`,
       withCredentials: true,
     });
-    await delay(0.1);
+    await delay(100);
     setEvents([...events, ...data]);
   }, [SERVER_URL, events]);
 
