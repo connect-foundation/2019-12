31911fbfb2546934f84c9440cbdc556bbe0320b4
FullOfOrange <foorgdev@gmail.com>
2019-12-02 16:10:02 +0900

    feat: remove unused values in header component
    header 컴포넌트에 전역 스토어가 생기면서 불필요해진 변수들을 제거함. (stories 추가)

--- client/src/components/organisms/Header/index.tsx
+++ client/src/components/organisms/Header/index.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import * as S from './style';
 import Btn from '../../atoms/Btn';
 import logo from '../../../assets/img/logo.svg';
 import ROUTES from '../../../commons/constants/routes';
-
 import { CREATE_EVENT } from '../../../commons/constants/string';
 
-interface Props {
-  userNameText: string;
-  onCreateEventBtnClickHandlerick?: () => void;
-  onAccountClickHandlerick?: () => void;
-}
+import { AccountState } from '../../../stores/accountStore';
 
-function Header({
-  userNameText,
-  onCreateEventBtnClickHandlerick = () => {},
-  onAccountClickHandlerick = () => {},
-}: Props): React.ReactElement {
+function Header(): React.ReactElement {
+  const account = useContext(AccountState);
+
   return (
     <S.Container>
       <Btn
@@ -26,19 +19,23 @@ function Header({
         styletype={'transparent'}
         content={CREATE_EVENT}
         to={ROUTES.EVENT_CREATE}
-        onClick={onCreateEventBtnClickHandlerick}
       />
 
       <S.StyledLink to={ROUTES.HOME}>
         <S.Img alt={'Logo'} src={logo} />
       </S.StyledLink>
+
       <Btn
         grow={false}
         fit={true}
         styletype={'transparent-border'}
-        content={userNameText}
-        to={ROUTES.LOGIN}
-        onClick={onAccountClickHandlerick}
+        content={
+          !account.isLogin
+            ? '로그인'
+            : `${account.firstName}${account.lastName}`
+        }
+        //원래 로그인이 성공했을 때에는 MyPage로 가야한다.
+        to={account.isLogin ? '/' : ROUTES.LOGIN}
       />
     </S.Container>
   );
