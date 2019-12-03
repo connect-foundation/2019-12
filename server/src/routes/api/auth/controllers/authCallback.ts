ba6fa3265ace8cd0d49f6426d45c459d3a1e4163
FullOfOrange <foorgdev@gmail.com>
2019-12-03 10:38:36 +0900

    feat: add error validation

--- server/src/routes/api/auth/controllers/authCallback.ts
+++ server/src/routes/api/auth/controllers/authCallback.ts
@@ -1,15 +1,14 @@
-import { Response } from 'express';
+import { Response, NextFunction } from 'express';
 
 const { CLIENT_URL } = process.env;
 
-export const authCallback = (req: any, res: Response) => {
+export const authCallback = (req: any, res: Response, next: NextFunction) => {
   try {
     const isUserExist: boolean = req.user ? req.user.exist : false;
 
     res.cookie('UID', req.user.token, {
       maxAge: 1000 * 60 * 60 * 60,
     });
-
     if (isUserExist) {
       const { state } = req.query;
       const { returnTo } = JSON.parse(Buffer.from(state, 'base64').toString());
@@ -22,7 +21,6 @@ export const authCallback = (req: any, res: Response) => {
       res.redirect(`${CLIENT_URL}/signup`);
     }
   } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
+    next(err);
   }
 };
