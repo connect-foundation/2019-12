bc03dfddf54a6528d32a6cd4bd964ea7631b919e
FullOfOrange <foorgdev@gmail.com>
2019-12-03 10:36:16 +0900

    feat: createUser now give generateJWT

--- server/src/routes/api/users/controllers/createUser.ts
+++ server/src/routes/api/users/controllers/createUser.ts
@@ -1,30 +1,39 @@
 import { Request, Response } from 'express';
 import { setUserInfo } from '../../../../services';
 
-export async function createUser(req: Request, res: Response) {
-  const isHaveAllKey = Object.keys(req.body).every(key => key !== undefined);
-  console.log(isHaveAllKey);
-  if (isHaveAllKey) {
-    const { exist, id, googleId, firstName, lastName, phoneNumber } = req.body;
+import { generateJWT } from '../../../../utils/jwt';
 
-    // Exist가 False일 경우, Token이 회원가입이 안되어 있음을 말함.
-    if (!exist) {
-      const result = await setUserInfo(
-        id,
-        googleId,
-        firstName,
-        lastName,
-        phoneNumber,
-      );
-      if (result[0] === 1) {
-        res.send({ message: 'Success Signup' });
-      } else {
-        res.status(400).send({ message: 'Cannot Signup' });
-      }
+export async function createUser(req: Request, res: Response) {
+  const {
+    exist,
+    id,
+    email,
+    googleId,
+    firstName,
+    lastName,
+    phoneNumber,
+  } = req.body;
+  // Exist가 False일 경우, Token이 회원가입이 안되어 있음을 말함.
+  if (!exist) {
+    const result = await setUserInfo(
+      id,
+      googleId,
+      firstName,
+      lastName,
+      phoneNumber,
+    );
+    if (result[0] === 1) {
+      const token = await generateJWT(true, id, googleId, email);
+      console.log(token, email);
+      res
+        .cookie('UID', token, {
+          maxAge: 1000 * 60 * 60 * 60,
+        })
+        .send({ message: 'Success Signup' });
     } else {
       res.status(400).send({ message: 'Cannot Signup' });
     }
   } else {
-    res.status(400).send({ message: 'Need more body data' });
+    res.status(400).send({ message: 'Cannot Signup' });
   }
 }
