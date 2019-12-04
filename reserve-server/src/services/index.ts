97e8150e50332ae8380f770e5ac53746a45a2fab
FullOfOrange <foorgdev@gmail.com>
2019-12-04 16:58:40 +0900

    feat: get ticket info method (redis data)
    레디스에 저장된 데이터를 뽑아올 수 있는 서비스를 외부로 추출함.

--- reserve-server/src/services/index.ts
+++ reserve-server/src/services/index.ts
@@ -1,3 +1,4 @@
 export { getTicketType, updateTicketType } from './ticketType';
 export { updateUserTicket, countUserTicket } from './userTicket';
 export { orderTransaction } from './transactions';
+export { TicketCacheData, getTicketCache} from './ticketCache';
