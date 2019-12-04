631d7b47b93d48458042a7b62aace62befd39c60
doong-jo <sdong001@gmail.com>
2019-12-05 00:24:50 +0900

    feat:  cypress setup (ts, webpack)

--- client/cypress/plugins/index.js
+++ client/cypress/plugins/index.js
@@ -1,17 +1,13 @@
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
+/* eslint-disable @typescript-eslint/no-var-requires */
+const webpack = require('@cypress/webpack-preprocessor');
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+module.exports = on => {
+  const options = {
+    // send in the options from your webpack.config.js, so it works the same
+    // as your app's code
+    webpackOptions: require('../webpack.config'),
+    watchOptions: {},
+  };
 
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  on('file:preprocessor', webpack(options));
 };
