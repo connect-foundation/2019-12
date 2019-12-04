1f53d776367ea89135501d7f2b01764817341aa9
doong-jo <sdong001@gmail.com>
2019-12-05 00:39:36 +0900

    test: add header e2e test

---
+++ client/cypress/integration/header.spec.ts
@@ -0,0 +1,16 @@
+/// <reference types="Cypress" />
+
+context('메인 페이지', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  it('이벤트 주최하기 클릭 시 이벤트 주최 페이지로 이동한다', () => {
+    cy.get('[data-testid=header-create]').click();
+    cy.location('pathname').should('eq', '/event/create');
+  });
+
+  it('로그인 버튼 클릭 시 로그인 페이지로 이동된다.', () => {
+    cy.get('[data-testid=header-account]').click();
+    cy.location('pathname').should('eq', '/login');
+  });
+});
