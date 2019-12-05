8a113dbd1ddf0313dea8f54e437b4b09a42b0c38
문종현 <dobest27@gmail.com>
2019-12-05 15:02:20 +0900

    refactor: http status code
    숫자로 하드코딩된 http 응답 상태를 http-status 모듈로 리팩토링

--- server/src/routes/api/events/middlewares/requestParamHandler.ts
+++ server/src/routes/api/events/middlewares/requestParamHandler.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { getEventById } from '../../../../services/events';
+import { NOT_FOUND } from 'http-status';
 
 export default async (
   req: Request,
@@ -12,6 +13,6 @@ export default async (
     req.event = await getEventById(eventId);
     next();
   } catch (error) {
-    res.sendStatus(404);
+    res.sendStatus(NOT_FOUND);
   }
 };
