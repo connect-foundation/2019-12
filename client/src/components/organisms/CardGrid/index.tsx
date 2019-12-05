7f75b7eea555a52ea00cba1806b325cc4ab075d2
Yongho Lee <21400564@handong.edu>
2019-12-05 22:09:34 +0900

    refactor: edit Main, CardGrid Component
    WHY
    - 전역 스토어의 사용에 따른 구조 변화
    
    WHAT
    - 바로 사용하는 것이 아니라 스토어를 이용해서 data 사용

--- client/src/components/organisms/CardGrid/index.tsx
+++ client/src/components/organisms/CardGrid/index.tsx
@@ -1,28 +1,38 @@
 import React from 'react';
 import * as S from './style';
-import Card from '../../molecules/Card';
-import { Event } from '../../../types/Event';
+import Card from 'components/molecules/Card';
+import { EventsState } from 'types/States';
 
 interface Props {
-  cards: Event[];
+  eventsState: EventsState;
   setRef?: React.Dispatch<React.SetStateAction<HTMLElement | null>>;
 }
 
-function CardGrid({ cards, setRef }: Props): React.ReactElement {
+function CardGrid({ eventsState, setRef }: Props): React.ReactElement {
   return (
     <>
       <S.CardGridContainer>
-        {cards.map(card => (
-          <Card
-            key={card.id}
-            imgSrc={card.mainImg}
-            date={card.startAt}
-            title={card.title}
-            host={card.user.lastName + card.user.firstName}
-            price={card.ticketType.price}
-            to={`/events/${card.id}`}
-          />
-        ))}
+        {eventsState.order!.map(index => {
+          const {
+            id,
+            mainImg,
+            startAt,
+            title,
+            user,
+            ticketType,
+          } = eventsState.events.get(index)!;
+          return (
+            <Card
+              key={id}
+              imgSrc={mainImg}
+              date={startAt}
+              title={title}
+              host={user.lastName + user.firstName}
+              price={ticketType.price}
+              to={`/events/${id}`}
+            />
+          );
+        })}
       </S.CardGridContainer>
       <div ref={setRef}></div>
     </>
--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,49 +1,58 @@
-import React, { useState, useCallback } from 'react';
+import React, { useCallback, useContext } from 'react';
 import axios from 'axios';
 
 import MainTemplate from './template';
 import MainBanner from 'components/organisms/MainBanner';
 import CardGrid from 'components/organisms/CardGrid';
-import { Event } from '../../types/Event';
-import { useIntersect } from '../../hooks';
-import delay from '../../utils/delay';
+import { Event } from 'types/Data';
+import { useIntersect } from 'hooks';
+import { EventsStoreState, EventsStoreAction } from 'stores/eventsStore';
+const { REACT_APP_SERVER_UR: SERVER_URL } = process.env;
+
+const fetchEvents = async (startAt: string) => {
+  const { data } = await axios({
+    method: 'GET',
+    url: `${SERVER_URL}/api/events?cnt=12${startAt}`,
+  });
+  const events = new Map<number, Event>();
+  const order = data.map((event: Event) => {
+    events.set(event.id, event);
+    return event.id;
+  });
+  return { events, order };
+};
 
 function Main(): React.ReactElement {
-  const [events, setEvents] = useState<Event[]>([]);
-  const { REACT_APP_SERVER_URL: SERVER_URL } = process.env;
+  const eventsState = useContext(EventsStoreState);
+  const { eventsDispather } = useContext(EventsStoreAction);
 
-  const fetchItems = useCallback(async () => {
-    const startAt =
-      events.length === 0
-        ? ''
-        : `&startAt=${events[events.length - 1].startAt}`;
-    const { data } = await axios({
-      method: 'GET',
-      url: `${SERVER_URL}/api/events?cnt=12${startAt}`,
-      withCredentials: true,
+  const getNextEvents = useCallback(async () => {
+    let startAt = '';
+    if (eventsState.order!.length !== 0) {
+      const lastItemIndex = eventsState.order!.slice(-1)[0];
+      startAt = `&startAt=${eventsState.events.get(lastItemIndex)!.startAt}`;
+    }
+    eventsDispather({
+      type: 'MAIN',
+      value: await fetchEvents(startAt),
     });
-    await delay(100);
-    setEvents([...events, ...data]);
-  }, [SERVER_URL, events]);
+  }, [eventsDispather, eventsState.events, eventsState.order]);
 
-  const [, setRef] = useIntersect(
-    async (
-      entry: IntersectionObserverEntry,
-      observer: IntersectionObserver,
-    ) => {
-      await fetchItems();
-    },
-    {
-      root: null,
-      threshold: 1.0,
-      rootMargin: '0% 0% 25% 0%',
-    },
-  );
+  const callback = async (
+    entry: IntersectionObserverEntry,
+    observer: IntersectionObserver,
+  ) => await getNextEvents();
+
+  const [, setRef] = useIntersect(callback, {
+    root: null,
+    threshold: 1.0,
+    rootMargin: '0% 0% 25% 0%',
+  });
 
   return (
     <MainTemplate
       mainBanner={<MainBanner />}
-      cardGrid={<CardGrid cards={events} setRef={setRef} />}
+      cardGrid={<CardGrid eventsState={eventsState} setRef={setRef} />}
     />
   );
 }
