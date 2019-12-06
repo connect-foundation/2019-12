be4e0658625d49284d9d417d230fc5eb3d618b62
doong-jo <sdong001@gmail.com>
2019-12-06 13:59:31 +0900

    fix: remove knob in global variable 전역 변수에 knob이 설정되어 어떤 스토리에 방문해도 표시되는 것을 컴포넌트 내부로 이동하여 해결

--- client/src/components/organisms/SignUpForm/index.stories.tsx
+++ client/src/components/organisms/SignUpForm/index.stories.tsx
@@ -14,54 +14,6 @@ import {
 export default {
   title: 'Organisms / SignUpForm',
 };
-const FormInputs = {
-  email: {
-    inputName: 'email',
-    captionContent: '이메일을 입력하세요',
-    value: text('이메일', ''),
-    labelProps: {
-      name: SIGNUP_EMAIL,
-      required: false,
-    },
-  },
-  firstName: {
-    inputName: 'firstName',
-    captionContent: '성을 입력하세요',
-    value: text('성', ''),
-    labelProps: {
-      name: SIGNUP_FIRST_NAME,
-      required: true,
-    },
-  },
-  lastName: {
-    inputName: 'lastName',
-    captionContent: '이름을 입력하세요',
-    value: text('이름', ''),
-    labelProps: {
-      name: SIGNUP_LAST_NAME,
-      required: true,
-    },
-  },
-  phoneNumber: {
-    inputName: 'phoneNumber',
-    captionContent: '올바른 휴대폰 번호가 아닙니다.',
-    value: text('휴대폰번호', ''),
-    labelProps: {
-      name: SIGNUP_PHONE_NUMBER,
-      required: true,
-    },
-  },
-  password: {
-    inputName: 'password',
-    captionContent:
-      '비밀번호는 대소문자, 특수문자, 숫자를 포함한 8글자 이상이어야 합니다.',
-    value: text('비밀번호', ''),
-    labelProps: {
-      name: SIGNUP_PASSSWORD,
-      required: true,
-    },
-  },
-};
 
 const Button = {
   children: text('컨텐츠', ''),
@@ -75,7 +27,47 @@ export const defaultInput: React.FC = () => {
       <h1 style={{ marginBottom: '4rem' }}>
         *state를 주입해야 하는 컴포넌트이기 때문에 아래의 Knob로 인풋 변경 가능
       </h1>
-      <SignUpForm FormInputs={FormInputs} Button={Button} />
+      <SignUpForm
+        FormInputs={{
+          email: {
+            inputName: 'email',
+            captionContent: '이메일을 입력하세요',
+            value: text('이메일', ''),
+            labelProps: {
+              name: SIGNUP_EMAIL,
+              required: false,
+            },
+          },
+          firstName: {
+            inputName: 'firstName',
+            captionContent: '성을 입력하세요',
+            value: text('성', ''),
+            labelProps: {
+              name: SIGNUP_FIRST_NAME,
+              required: true,
+            },
+          },
+          lastName: {
+            inputName: 'lastName',
+            captionContent: '이름을 입력하세요',
+            value: text('이름', ''),
+            labelProps: {
+              name: SIGNUP_LAST_NAME,
+              required: true,
+            },
+          },
+          phoneNumber: {
+            inputName: 'phoneNumber',
+            captionContent: '올바른 휴대폰 번호가 아닙니다.',
+            value: text('휴대폰번호', ''),
+            labelProps: {
+              name: SIGNUP_PHONE_NUMBER,
+              required: true,
+            },
+          },
+        }}
+        Button={Button}
+      />
     </>
   );
 };
