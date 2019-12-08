1d2088731ebadf5c412e6a934127415590523978
FullOfOrange <foorgdev@gmail.com>
2019-12-08 18:25:40 +0900

    refactor: add error message in redis logic
    레디스를 이용해서 로직을 처리하는 부분도 동일한 오류메시지를 뿜도록 변경함.

--- reserve-server/src/routes/api/controllers/checkTicket.ts
+++ reserve-server/src/routes/api/controllers/checkTicket.ts
@@ -10,11 +10,23 @@ export default async (req: any, res: Response, next: NextFunction) => {
     const { isBlock, salesEndAt, salesStartAt } = (await getTicketCache(
       ticketId,
     )) as TicketCacheData;
-    if (isBlock === '0' && time > +salesStartAt && time < +salesEndAt)
-      return next();
 
-    return res.status(FORBIDDEN).send({ message: 'failure' });
+    if (isBlock === '1')
+      return res.status(FORBIDDEN).send({
+        state: 1,
+        message: 'ticket sold out',
+      }); // 티켓이 현재 다 팔림
+
+    if (time < +salesStartAt || time > +salesEndAt)
+      return res.status(FORBIDDEN).send({
+        state: 0,
+        message: 'wrong date',
+      }); // 티켓 구매 시간이 아님
+
+    next();
   } catch (err) {
-    return res.status(NOT_FOUND).send({ message: 'no Ticket Exist' });
+    return res.status(NOT_FOUND).send({
+      message: 'wrong number of ticket',
+    });
   }
 };
