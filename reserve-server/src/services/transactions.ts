2113ac5d9cb2b3858d25a6937080dad489fcf4e5
FullOfOrange <foorgdev@gmail.com>
2019-12-08 18:24:02 +0900

    refactor: add transaction failure handling with throw
    트렌젝션에서 에러의 여부를 확인할 수 있도록 message 와 state 를 리턴해줌.

--- reserve-server/src/services/transactions.ts
+++ reserve-server/src/services/transactions.ts
@@ -20,20 +20,34 @@ export const orderTransaction = async (
     countUserTicket(transaction, userId, ticketId),
     getTicketType(transaction, ticketId),
   ]);
-  if (ticket.leftCnt === 0) redis.hset(`${ticketId}`, 'isBlock', '1');
 
-  // redis 가 죽었을 때를 대비해서 시간 검사 로직 추가
-  const time = Date.now();
-  if (
-    ticket &&
-    ticket.maxCntPerPerson >= userTicketNum + orderTicketNum &&
-    time > +ticket.salesStartAt &&
-    time < +ticket.salesEndAt
-  ) {
-    // 구매할 티켓이 존재할 때, 유저가 산 티켓 개수가 살 수 있는 티켓 개수보다 적을경우,
-    await updateUserTicket(transaction, userId, ticketId, orderTicketNum);
-    await updateTicketType(transaction, ticketId, orderTicketNum);
-  } else {
-    throw new Error('transaction fail');
+  if (ticket.leftCnt === 0) {
+    redis.hset(`${ticketId}`, 'isBlock', '1');
+    throw {
+      state: 1,
+      message: 'ticket sold out',
+    }; // 티켓이 다 팔렸을 경우
   }
+  if (!ticket)
+    throw {
+      state: 404,
+      message: 'wrong number of ticket',
+    };
+
+  if (ticket.maxCntPerPerson < userTicketNum + orderTicketNum)
+    throw {
+      state: 2,
+      message: 'limit exceed ticket per person',
+    }; // 티켓의 구매 개수를 초과함
+
+  const time = Date.now();
+  if (time < +ticket.salesStartAt || time > +ticket.salesEndAt)
+    throw {
+      state: 0,
+      message: 'wrong date',
+    }; // 티켓이 아직 열리지 않음
+
+  // 구매할 티켓이 존재할 때, 유저가 산 티켓 개수가 살 수 있는 티켓 개수보다 적을경우,
+  await updateUserTicket(transaction, userId, ticketId, orderTicketNum);
+  await updateTicketType(transaction, ticketId, orderTicketNum);
 };
