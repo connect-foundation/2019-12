0a59b32297bbe849da60eecdcbb7425d1f13a45d
FullOfOrange <foorgdev@gmail.com>
2019-12-08 23:02:00 +0900

    refactor: change validator form
    종현님이 알려주신 방식으로 body를 validation 함

--- server/src/routes/api/users/index.ts
+++ server/src/routes/api/users/index.ts
@@ -1,11 +1,17 @@
 import * as express from 'express';
 import * as controllers from './controllers';
 import * as validators from './validators';
+import { badRequestHandler } from '../../../utils/errorHandler';
 //import * as validator from './validators';
 
 const router = express.Router();
 
 router.post('/:userId', controllers.getUser);
-router.post('/', validators.createUser, controllers.createUser);
+router.post(
+  '/',
+  validators.createUser,
+  badRequestHandler,
+  controllers.createUser,
+);
 
 export default router;
