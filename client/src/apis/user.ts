ff17a9330f80f5d28503ca8deca91012b437e5bf
doong-jo <sdong001@gmail.com>
2019-12-09 00:08:18 +0900

    feat: write user api module

---
+++ client/src/apis/user.ts
@@ -0,0 +1,13 @@
+import { mainAxios } from 'libs/axios';
+
+export const createUser = (
+  id: number,
+  googleId: number,
+  email: string,
+  firstName: string,
+  lastName: string,
+  phoneNumber: number,
+) =>
+  mainAxios.post('users', {
+    data: { id, googleId, email, firstName, lastName, phoneNumber },
+  });
