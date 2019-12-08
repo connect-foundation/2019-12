a1e548b9eebdc96c45c00b72223ad4ee42aba961
FullOfOrange <foorgdev@gmail.com>
2019-12-08 17:22:02 +0900

    refactor: add time validation in ticket buy transaction

--- reserve-server/src/services/transactions.ts
+++ reserve-server/src/services/transactions.ts
@@ -22,7 +22,14 @@ export const orderTransaction = async (
   ]);
   if (ticket.leftCnt === 0) redis.hset(`${ticketId}`, 'isBlock', '1');
 
-  if (ticket && ticket.maxCntPerPerson >= userTicketNum + orderTicketNum) {
+  // redis 가 죽었을 때를 대비해서 시간 검사 로직 추가
+  const time = Date.now();
+  if (
+    ticket &&
+    ticket.maxCntPerPerson >= userTicketNum + orderTicketNum &&
+    time > +ticket.salesStartAt &&
+    time < +ticket.salesEndAt
+  ) {
     // 구매할 티켓이 존재할 때, 유저가 산 티켓 개수가 살 수 있는 티켓 개수보다 적을경우,
     await updateUserTicket(transaction, userId, ticketId, orderTicketNum);
     await updateTicketType(transaction, ticketId, orderTicketNum);
