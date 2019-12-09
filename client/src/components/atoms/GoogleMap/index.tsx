9d95f445c9bb8e7acb5117e62af6649aeb6cd328
Yongho Lee <21400564@handong.edu>
2019-12-09 14:15:29 +0900

    fix: fix a bug about GoogleMap
    WHY
    - center가 변경되어도 초기 위치를 가리키는 버그가 있음
    
    WHAT
    - 조건부 렌더링으로 초기 center일 때는 렌더링을 하지 않도록 해결

--- client/src/components/atoms/GoogleMap/index.tsx
+++ client/src/components/atoms/GoogleMap/index.tsx
@@ -12,25 +12,28 @@ const {
 const googleApiKey =
   REACT_APP_GOOGLE_MAP_API_KEY || STORYBOOK_GOOGLE_MAP_API_KEY;
 const defaultZoom = 17;
-const defaultCenter = { lat: 37.5662952, lng: 126.9779451 };
 
 interface Props {
   latitude: number;
   longitude: number;
 }
 
-function GoogleMap({ latitude, longitude }: Props): React.ReactElement {
+function GoogleMap({
+  latitude: lat,
+  longitude: lng,
+}: Props): React.ReactElement {
   return (
     <S.Container>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: `${googleApiKey}` }}
-        center={{ lat: latitude, lng: longitude }}
-        defaultCenter={defaultCenter}
-        defaultZoom={defaultZoom}
-        draggable={false}
-      >
-        <S.PinIcon alt={'pin'} height={'3rem'} src={Pin} />
-      </GoogleMapReact>
+      {lat && lng && (
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: `${googleApiKey}` }}
+          center={{ lat, lng }}
+          defaultZoom={defaultZoom}
+          draggable={false}
+        >
+          <S.PinIcon alt={'pin'} height={'3rem'} src={Pin} />
+        </GoogleMapReact>
+      )}
     </S.Container>
   );
 }
