70c1fcb820589b44df927f774f2759af3a765339
doong-jo <sdong001@gmail.com>
2019-12-10 06:49:04 +0900

    refactor: use constants

--- client/src/pages/EventJoin/view.tsx
+++ client/src/pages/EventJoin/view.tsx
@@ -10,6 +10,12 @@ import {
   RESERVE_COMPLETE,
   RESERVE_BAD_FAIL,
   RESERVE_MIN_FAIL,
+  JOIN_STEP_CHOICE,
+  JOIN_STEP_PURCHASE,
+  TICKET_CHOICE_TITLE,
+  TICKET_PURCHASE_TITLE,
+  TOTAL_PRICE_LABEL,
+  TICKET_PURCHASE_BTN,
 } from 'commons/constants/string';
 import ROUTES from 'commons/constants/routes';
 import * as S from './style';
@@ -20,6 +26,7 @@ interface Props {
 }
 
 const minTicketCount = 1;
+const steps = [JOIN_STEP_CHOICE, JOIN_STEP_PURCHASE];
 const ticketData = {
   id: 1,
   eventId: 2,
@@ -33,6 +40,37 @@ const ticketData = {
   salesStartAt: '2019-11-04T15:00:00.000Z',
   salesEndAt: '2019-11-28T14:00:00.000Z',
   refundEndAt: '2019-11-28T14:00:00.000Z',
+};
+const eventData = {
+  id: 1,
+  title: '이벤트 제목이랍니다.',
+  startAt: '2019-11-04T15:00:00.000Z',
+  endAt: '2019-11-05T15:00:00.000Z',
+  place: '위플레이스 강남점(서울시 강남구 강남대로 340 경원빌딩 3층)',
+  address: '서울시 강남구 강남대로 340',
+  placeDesc: '',
+  location: {
+    latitude: 37.5662952,
+    longitude: 126.9779451,
+  },
+  mainImg: '',
+  desc: '',
+
+  ticketType: {
+    id: 1,
+    eventId: 1,
+    name: '일반 입장권',
+    desc: '',
+    price: 10000,
+    quantity: 0,
+    leftCnt: 20,
+    isPublicLeftCnt: false,
+    maxCntPerPerson: 10,
+    salesStartAt: '2019-11-04T15:00:00.000Z',
+    salesEndAt: '2019-11-28T14:00:00.000Z',
+    refundEndAt: '2019-11-28T14:00:00.000Z',
+  },
+  user: { id: 0, lastName: '조', firstName: '성동', profileImgUrl: '' },
 };
 
 function EventJoin({ eventId }: Props): React.ReactElement {
