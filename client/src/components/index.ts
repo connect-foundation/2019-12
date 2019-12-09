4473aeae1452daa1fe5969782ba626d0684f286c
doong-jo <sdong001@gmail.com>
2019-12-09 18:35:02 +0900

    feat: add StepList component in index of components

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -15,6 +15,7 @@ export { default as FormInput } from './molecules/FormInput';
 export { default as IconBtn } from './molecules/IconBtn';
 export { default as IconLabel } from './molecules/IconLabel';
 export { default as ImgBtn } from './molecules/ImgBtn';
+export { default as StepList } from './molecules/StepList';
 
 export { default as CardGrid } from './organisms/CardGrid';
 export { default as EventHeader } from './organisms/EventHeader';
