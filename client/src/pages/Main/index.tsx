be3f01767c08cf5fe3789077e50332a59593c4c5
Yongho Lee <21400564@handong.edu>
2019-12-09 20:25:50 +0900

    refactor: edit MainPage Logic

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,25 +1,12 @@
 import React, { useCallback, useContext } from 'react';
-import { getEvents } from 'apis';
 
 import MainTemplate from './template';
 import { MainBanner, CardGrid } from 'components';
-import { EventDetail } from 'types/Data';
 import { useIntersect } from 'hooks';
 import { EventsStoreState, EventsStoreAction } from 'stores/eventsStore';
 import { EventsState } from 'types/States';
 
 const requestEventNum = 12;
-
-const fetchEvents = async (startAt: string) => {
-  const { data } = await getEvents(requestEventNum, startAt);
-
-  const events = new Map<number, EventDetail>();
-  const order = data.map((event: EventDetail) => {
-    events.set(event.id, event);
-    return event.id;
-  });
-  return { events, order };
-};
 
 const getStartAt = (eventsState: EventsState) => {
   let startAt = '';
@@ -32,30 +19,32 @@ const getStartAt = (eventsState: EventsState) => {
 
 function Main(): React.ReactElement {
   const eventsState = useContext(EventsStoreState);
-  const { eventsDispather } = useContext(EventsStoreAction);
+  const { eventFetchDispatcher } = useContext(EventsStoreAction);
 
   const getNextEvents = useCallback(
     async function() {
       const startAt = getStartAt(eventsState);
-
-      eventsDispather({
-        type: 'MAIN',
-        value: await fetchEvents(startAt),
+      eventFetchDispatcher({
+        type: 'EVENTS',
+        params: {
+          cnt: requestEventNum,
+          startAt,
+        },
       });
     },
-    [eventsDispather, eventsState],
+    [eventsState, eventFetchDispatcher],
   );
-
-  const callback = async (
-    entry: IntersectionObserverEntry,
-    observer: IntersectionObserver,
-  ) => await getNextEvents();
 
   const [, setRef] = useIntersect(callback, {
     root: null,
     threshold: 1.0,
-    rootMargin: '0% 0% 25% 0%',
+    rootMargin: '0% 0% 50% 0%',
   });
+
+  async function callback() {
+    setRef(null);
+    await getNextEvents();
+  }
 
   return (
     <MainTemplate
