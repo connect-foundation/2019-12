7618f313b245a065bf7fe1ee93f2022ca289f9f5
Yongho Lee <21400564@handong.edu>
2019-12-09 22:27:19 +0900

    feat: edit eventsStore more abstractly
    WHY
    - 아래에서 올라오는 요청을 보다 추상화시킴

--- client/src/stores/eventsStore.tsx
+++ client/src/stores/eventsStore.tsx
@@ -6,15 +6,12 @@ import React, {
   useState,
   useEffect,
 } from 'react';
-import { OK, NOT_FOUND, INTERNAL_SERVER_ERROR } from 'http-status';
 
 import { EventsAction } from 'types/Actions';
 import { EventsState } from 'types/States';
 import { EventsReducer } from 'types/CustomHooks';
 import { eventsReducer, defaultEventsState } from 'hooks';
-
 import { getEvents, getEvent } from 'apis';
-import { EventDetail } from 'types/Data';
 
 interface EventFetch {
   type: string;
@@ -49,45 +46,28 @@ function EventsProvider({ children }: { children: React.ReactElement }) {
       case 'EVENTS':
         (async () => {
           const { cnt, startAt } = eventFetch.params;
-          const { data } = await getEvents(cnt, startAt);
-          const events = new Map<number, EventDetail>();
-          const order = data.map((event: EventDetail) => {
-            events.set(event.id, event);
-            return event.id;
-          });
+          const { type, events, order, status } = await getEvents(cnt, startAt);
           eventsDispather({
-            type: 'EVENTS',
-            value: { events, order, status: OK },
+            type,
+            value: {
+              events,
+              order,
+              status,
+            },
           });
         })();
         break;
       case 'EVENT':
         (async function fetchData() {
           const { eventId } = eventFetch.params;
-          try {
-            const { data } = await getEvent(eventId);
-            const events = new Map([[data.id, data]]);
-            eventsDispather({
-              type: 'EVENT',
-              value: { events, status: OK },
-            });
-          } catch (err) {
-            if (err.response && err.response.status === NOT_FOUND) {
-              eventsDispather({
-                type: String(NOT_FOUND),
-                value: {
-                  status: NOT_FOUND,
-                },
-              });
-            } else {
-              eventsDispather({
-                type: String(INTERNAL_SERVER_ERROR),
-                value: {
-                  status: INTERNAL_SERVER_ERROR,
-                },
-              });
-            }
-          }
+          const { type, events, status } = await getEvent(eventId);
+          eventsDispather({
+            type,
+            value: {
+              events,
+              status,
+            },
+          });
         })();
         break;
     }
