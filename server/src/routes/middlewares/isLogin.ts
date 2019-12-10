1b0a81e4acc62c8d4cb7627362bf6f10273b7d62
FullOfOrange <foorgdev@gmail.com>
2019-12-10 17:40:49 +0900

    feat: update login check logic & add test code for login check

--- server/src/routes/middlewares/isLogin.ts
+++ server/src/routes/middlewares/isLogin.ts
@@ -6,7 +6,8 @@ export default async (req: any, res: Response, next: NextFunction) => {
   const token = req.cookies.UID;
   try {
     if (!token) throw new Error('no token');
-    const { id } = await verifyJWT(token);
+    const { exist, id } = await verifyJWT(token);
+    if (!exist) throw new Error('no user exist');
     req.user = { id };
     next();
   } catch (err) {
