49eaba70a6848edd4fb826b573fce28cf7582cc4
FullOfOrange <foorgdev@gmail.com>
2019-12-10 18:02:03 +0900

    feat: remove unused value
    사용하지않는 변수를 없앰

--- server/src/routes/api/users/controllers/getUserTickets.ts
+++ server/src/routes/api/users/controllers/getUserTickets.ts
@@ -8,21 +8,22 @@ export async function getUserTicket(req: Request, res: Response) {
     if (!result)
       return res.status(BAD_REQUEST).send({ message: 'Cannot get data' });
 
-    const data1 = result.reduce<Array<any>>((acc, cur) => {
-      const current: any = cur.get({ plain: true });
-      const { ticketType, ...userTickets } = current;
-      const { event, ...ticketTypes } = ticketType;
-      const { desc, latitude, longitude, isPublic, ...events } = event;
-      const obj = {
-        ...events,
-        ticket: ticketTypes,
-        userTickets: [],
-      };
-      if (!acc.includes(obj)) acc.push(obj);
-      acc.find(cur => cur.id === obj.id).userTickets.push(userTickets);
-      return acc;
-    }, []);
-    res.send(data1);
+    res.send(
+      result.reduce<Array<any>>((acc, cur) => {
+        const current: any = cur.get({ plain: true });
+        const { ticketType, ...userTickets } = current;
+        const { event, ...ticketTypes } = ticketType;
+        const { desc, latitude, longitude, isPublic, ...events } = event;
+        const obj = {
+          ...events,
+          ticket: ticketTypes,
+          userTickets: [],
+        };
+        if (!acc.includes(obj)) acc.push(obj);
+        acc.find(cur => cur.id === obj.id).userTickets.push(userTickets);
+        return acc;
+      }, []),
+    );
   } catch (err) {
     res.sendStatus(BAD_REQUEST);
   }
