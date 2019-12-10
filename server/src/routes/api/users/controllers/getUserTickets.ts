be876a18f8f5d12fc4df6f326865cf6bf8739210
FullOfOrange <foorgdev@gmail.com>
2019-12-10 19:24:39 +0900

    refactor: remove types & seperate controller

--- server/src/routes/api/users/controllers/getUserTickets.ts
+++ server/src/routes/api/users/controllers/getUserTickets.ts
@@ -1,28 +1,12 @@
 import { Request, Response } from 'express';
 import { getUserTicketsByUserId } from 'services';
-import { BAD_REQUEST } from 'http-status';
+import { BAD_REQUEST, NO_CONTENT } from 'http-status';
 
 export async function getUserTicket(req: Request, res: Response) {
   try {
     const result = await getUserTicketsByUserId(+req.user!.id);
-    if (!result)
-      return res.status(BAD_REQUEST).send({ message: 'Cannot get data' });
-
-    const userTickets = result.reduce<Array<any>>((acc, cur) => {
-      const current: any = cur.get({ plain: true });
-      const { ticketType, ...userTickets } = current;
-      const { event, ...ticketTypes } = ticketType;
-      const { desc, latitude, longitude, isPublic, ...events } = event;
-      const obj = {
-        ...events,
-        ticket: ticketTypes,
-        userTickets: [],
-      };
-      if (!acc.includes(obj)) acc.push(obj);
-      acc.find(cur => cur.id === obj.id).userTickets.push(userTickets);
-      return acc;
-    }, []);
-    if (!userTickets.length) res.status(204);
+    const userTickets = result;
+    if (!userTickets.length) res.status(NO_CONTENT);
     res.send(userTickets);
   } catch (err) {
     res.sendStatus(BAD_REQUEST);
--- server/src/services/userTickets.ts
+++ server/src/services/userTickets.ts
@@ -1,9 +1,79 @@
 import { UserTicket, Event, TicketType } from 'models';
 import { WhereOptions, FindAttributeOptions, Includeable } from 'sequelize';
 
+interface UserTicketEvent {
+  id: number;
+  userId: number;
+  title: string;
+  startAt: Date;
+  endAt: Date;
+  place: string;
+  address: string;
+  placeDesc: string;
+  mainImg: string;
+  ticket: TicketRaw;
+  userTickets: Array<UserTicketRaw>;
+}
+interface EventRaw {
+  id: number;
+  userId: number;
+  title: string;
+  startAt: Date;
+  endAt: Date;
+  place: string;
+  address: string;
+  placeDesc: string;
+  mainImg: string;
+  desc: any;
+  latitude: any;
+  longitude: any;
+  isPublic: any;
+}
+interface TicketRaw {
+  id: number;
+  eventId: number;
+  name: string;
+  desc: string;
+  price: number;
+  quantity: number;
+  leftCnt: number;
+  isPublicLeftCnt: boolean;
+  maxCntPerPerson: number;
+  salesStartAt: Date;
+  salesEndAt: Date;
+  refundEndAt: Date;
+  event?: EventRaw;
+}
+interface UserTicketRaw {
+  id: number;
+  ticketTypeId: number;
+  userId: number;
+  isAttendance: boolean;
+  createdAt: Date;
+  ticketType?: TicketRaw;
+}
+
+function userTicketReducer(
+  acc: Array<UserTicketEvent>,
+  cur: UserTicket,
+): Array<UserTicketEvent> {
+  const current = cur.get({ plain: true }) as UserTicket;
+  const { ticketType, ...userTickets } = current;
+  const { event, ...ticketTypes } = ticketType!;
+  const { desc, latitude, longitude, isPublic, ...events } = event!;
+  const obj = {
+    ...events,
+    ticket: ticketTypes,
+    userTickets: [],
+  } as UserTicketEvent;
+  if (!acc.includes(obj)) acc.push(obj);
+  acc.find(cur => cur.id === obj.id)!.userTickets.push(userTickets);
+  return acc;
+}
+
 export async function getUserTicketsByUserId(
   userId: number,
-): Promise<UserTicket[]> {
+): Promise<Array<UserTicketEvent>> {
   const attributes: FindAttributeOptions = {
     exclude: ['isPublic', 'updatedAt'],
   };
@@ -24,5 +94,6 @@ export async function getUserTicketsByUserId(
       ],
     },
   ];
-  return await UserTicket.findAll({ include, attributes, where });
+  const result = await UserTicket.findAll({ include, attributes, where });
+  return result.reduce<Array<UserTicketEvent>>(userTicketReducer, []);
 }
