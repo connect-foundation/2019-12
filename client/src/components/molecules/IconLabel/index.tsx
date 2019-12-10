21c33be6994ca492aef5a5e27693d20e64c6549c
doong-jo <sdong001@gmail.com>
2019-12-10 18:40:47 +0900

    refactor: rename IconLabel interface

--- client/src/components/molecules/IconLabel/index.tsx
+++ client/src/components/molecules/IconLabel/index.tsx
@@ -3,15 +3,12 @@ import React from 'react';
 import * as S from './style';
 import Icon, { Props as IconProps } from 'components/atoms/Icon';
 
-export interface IconLabelProps {
+export interface Props {
   iconProps: IconProps;
   labelContent: string;
 }
 
-function IconLabel({
-  iconProps,
-  labelContent,
-}: IconLabelProps): React.ReactElement {
+function IconLabel({ iconProps, labelContent }: Props): React.ReactElement {
   return (
     <S.Container>
       <Icon {...iconProps} />
