f897c718a1325dee579e567974d48ffe2f1bc1a5
FullOfOrange <foorgdev@gmail.com>
2019-12-10 16:34:05 +0900

    feat: add login checking middleware
    로그인을 체크하는 미들웨어 추가

---
+++ server/src/routes/middlewares/isLogin.ts
@@ -0,0 +1,16 @@
+import { Response, NextFunction } from 'express';
+import { verifyJWT } from 'utils/jwt';
+import { UNAUTHORIZED } from 'http-status';
+
+export default async (req: any, res: Response, next: NextFunction) => {
+  const token = req.cookies.UID;
+  try {
+    const { id } = await verifyJWT(token);
+    req.user = { id };
+    next();
+  } catch (err) {
+    res.status(UNAUTHORIZED).send({
+      message: 'unauthorized',
+    });
+  }
+};
