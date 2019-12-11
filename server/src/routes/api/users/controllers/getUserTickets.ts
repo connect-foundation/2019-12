e717df8030396cdf6337844ce68886d63c822d6f
FullOfOrange <foorgdev@gmail.com>
2019-12-11 11:44:05 +0900

    refactor: throw Error when user obj gone

--- server/src/routes/api/users/controllers/getUserTickets.ts
+++ server/src/routes/api/users/controllers/getUserTickets.ts
@@ -4,7 +4,8 @@ import { BAD_REQUEST, NO_CONTENT } from 'http-status';
 
 export async function getUserTicket(req: Request, res: Response) {
   try {
-    const result = await getUserTicketsByUserId(+req.user!.id);
+    if (!req.user) throw new Error('no user id');
+    const result = await getUserTicketsByUserId(req.user.id);
     const userTickets = result;
     if (!userTickets.length) res.status(NO_CONTENT);
     res.send(userTickets);
