41cae4f12a79f469e783debd87a3f1fcec4f479c
문종현 <dobest27@gmail.com>
2019-12-12 00:55:02 +0900

    feat: add limitSizeSingleFile middleware
    업로드하는 파일의 사이즈를 제한하는 미들웨어 구현

--- server/src/routes/middlewares/index.ts
+++ server/src/routes/middlewares/index.ts
@@ -1,3 +1,4 @@
+export { default as limitSizeSingleFile } from './limitSizeSingleFile';
 export { default as requireSingleFile } from './requireSingleFile';
 export { default as requireLogin } from './requireLogin';
 export { default as singleFileUpload } from './singleFileUpload';
