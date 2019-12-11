2fd9e201983293f9542b10c36951f1361ef667e3
FullOfOrange <foorgdev@gmail.com>
2019-12-11 14:50:53 +0900

    refactor: send internal error handler when error catched
    에러가 잡히면 바로 서버 인터널 에러로 넘어가도록 설정함.

--- server/src/routes/api/users/controllers/deleteUserTicket.ts
+++ server/src/routes/api/users/controllers/deleteUserTicket.ts
@@ -1,14 +1,18 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { deleteUserTicketById } from 'services';
-import { BAD_REQUEST, NO_CONTENT, NOT_FOUND } from 'http-status';
+import { NO_CONTENT, NOT_FOUND } from 'http-status';
 
-export async function deleteUserTicket(req: Request, res: Response) {
+export async function deleteUserTicket(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
   try {
     const { ticketId } = req.body;
     const result = await deleteUserTicketById(ticketId, +req.user!.id);
     if (!result) return res.sendStatus(NOT_FOUND);
     res.sendStatus(NO_CONTENT);
   } catch (err) {
-    res.sendStatus(BAD_REQUEST);
+    next(err);
   }
 }
--- server/src/routes/api/users/controllers/getUserTickets.ts
+++ server/src/routes/api/users/controllers/getUserTickets.ts
@@ -1,14 +1,17 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { getUserTicketsByUserId } from 'services';
-import { BAD_REQUEST, NO_CONTENT } from 'http-status';
+import { NO_CONTENT } from 'http-status';
 
-export async function getUserTicket(req: Request, res: Response) {
+export async function getUserTicket(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
   try {
-    if (!req.user) throw new Error('no user id');
-    const userTickets = await getUserTicketsByUserId(+req.user.id);
+    const userTickets = await getUserTicketsByUserId(req.user!.id);
     if (!userTickets.length) res.status(NO_CONTENT);
     res.send(userTickets);
   } catch (err) {
-    res.sendStatus(BAD_REQUEST);
+    next(err);
   }
 }
