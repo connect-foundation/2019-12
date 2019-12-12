19bad70bfe144d91f66e268f0586bac876e559e1
문종현 <dobest27@gmail.com>
2019-12-12 22:30:34 +0900

    test: event create API

--- server/test/api/event.spec.ts
+++ server/test/api/event.spec.ts
@@ -5,9 +5,11 @@ import { Secret } from 'jsonwebtoken';
 import { generateJWT } from '../../src/utils/jwt';
 import { sequelize } from '../../src/utils/sequelize';
 import { Event } from '../../src/models';
-import { cloneDeep } from 'lodash';
+import { clone } from 'lodash';
+import { resolve } from 'path';
 import {
   OK,
+  CREATED,
   NO_CONTENT,
   NOT_FOUND,
   UNAUTHORIZED,
@@ -20,10 +22,12 @@ const setHeader = (token: Secret) => {
     Accept: 'application/json',
   };
 };
+let token: Secret;
 
 beforeAll(async () => {
   sequelize.options.logging = false;
   await sequelize.sync();
+  token = await generateJWT(true, 2, 1, '1234@gmail.com');
 });
 
 afterAll(() => {
@@ -131,57 +135,94 @@ describe('GET /api/events/coordinate', () => {
   });
 });
 
-describe('POST /api/events', async () => {
-  const createEventBody = {
+describe('POST /api/events', () => {
+  const defaultImage = resolve(__dirname, './file/createEvent/normal.jpg');
+  const defaultData: Record<string, string | boolean | number> = {
     isPublic: true,
     title: '이벤트의 제목',
-    startAt: new Date('2020-01-01 13:00:00'),
-    endAt: new Date('2020-02-01 13:00:00'),
+    startAt: '2200-01-01 13:00:00',
+    endAt: '2200-05-01 13:00:00',
     place: '패스트파이브 강남 4호점',
     address: '서울시 강남구',
     placeDesc: '주차 불가',
     desc: '설명',
-    ticket: {
-      name: '일반',
-      desc: '일반',
-      quantity: 30,
-      isPublicLeftCnt: false,
-      maxCntPerPerson: 10,
-    },
+    latitude: 37.5662952,
+    longitude: 126.9779451,
+    'ticket[name]': '티켓 이름',
+    'ticket[desc]': '티켓 설명',
+    'ticket[quantity]': 30,
+    'ticket[isPublicLeftCnt]': false,
+    'ticket[maxCntPerPerson]': 10,
+    'ticket[price]': 10000,
+    'ticket[salesStartAt]': '2019-12-15 10:00:00',
+    'ticket[salesEndAt]': '2019-12-17 13:00:00',
+    'ticket[refundEndAt]': '2019-12-20 13:00:00',
   };
 
-  it('정상적으로 응답', async () => {
-    await request(app)
+  function getRequest(
+    data: Record<string, string | boolean | number> = {},
+    imagePath: string = defaultImage,
+    loggedIn = true,
+  ) {
+    const req = request(app)
       .post('/api/events')
-      .send(createEventBody)
-      .expect(OK);
+      .type('form')
+      .attach('mainImg', imagePath);
+
+    if (loggedIn) req.set(setHeader(token));
+
+    const form = Object.assign(clone(defaultData), data);
+    for (const key in form) {
+      req.field(key, form[key]);
+    }
+
+    return req;
+  }
+
+  it('정상적으로 응답', async () => {
+    await getRequest().expect(CREATED);
+  });
+
+  it('로그인이 되어있지 않으면 401 응답', async () => {
+    await getRequest({}, undefined, false).expect(UNAUTHORIZED);
+  });
+
+  it('startAt 이 오늘보다 전이면 400 응답', async () => {
+    await getRequest({ startAt: '1900-03-01 13:00:00' }).expect(BAD_REQUEST);
   });
 
   it('startAt 이 endAt 보다 크면 400 응답', async () => {
-    const body = cloneDeep(createEventBody);
-    body.startAt = new Date('2020-03-01 13:00:00');
-    await request(app)
-      .post('/api/events')
-      .send(body)
-      .expect(BAD_REQUEST);
+    await getRequest({ startAt: '2201-03-01 13:00:00' }).expect(BAD_REQUEST);
   });
 
   it('ticket 의 maxCntPerPerson 가 quantity 보다 크면 400 응답', async () => {
-    const body = cloneDeep(createEventBody);
-    body.ticket.maxCntPerPerson = body.ticket.quantity + 5;
-    await request(app)
-      .post('/api/events')
-      .send(body)
-      .expect(BAD_REQUEST);
+    await getRequest({ 'ticket[maxCntPerPerson]': 200 }).expect(BAD_REQUEST);
   });
 
-  it('필수 항목이 없을 경우 400 응답', async () => {
-    const body = cloneDeep(createEventBody);
-    delete body.title;
-    await request(app)
-      .post('/api/events')
-      .send(body)
-      .expect(BAD_REQUEST);
+  it('ticket 의 salesStartAt 가 salesEndAt 보다 늦으면 400 응답', async () => {
+    await getRequest({ 'ticket[salesEndAt]': '2201-03-01 13:00:00' }).expect(
+      BAD_REQUEST,
+    );
+  });
+
+  it('ticket 의 refundEndAt 이 refundEndAt 보다 늦으면 400 응답', async () => {
+    await getRequest({ 'ticket[salesStartAt]': '2201-03-01 13:00:00' }).expect(
+      BAD_REQUEST,
+    );
+  });
+
+  it('mainImg 의 크기가 10MB 이상의 크기면 400 응답', async () => {
+    await getRequest(
+      {},
+      resolve(__dirname, './file/createEvent/big.jpg'),
+    ).expect(BAD_REQUEST);
+  });
+
+  it('mainImg 의 파일이 이미지 파일이 아니면 400 응답', async () => {
+    await getRequest(
+      {},
+      resolve(__dirname, './file/createEvent/text.txt'),
+    ).expect(BAD_REQUEST);
   });
 });
 
