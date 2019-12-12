132c52aed50b623d2d4480b7a118d22d94ce8796
FullOfOrange <foorgdev@gmail.com>
2019-12-12 15:03:18 +0900

    feat: add router

--- server/src/routes/api/events/index.ts
+++ server/src/routes/api/events/index.ts
@@ -3,14 +3,20 @@ import { Router } from 'express';
 import * as controllers from './controllers';
 import * as validators from './validators';
 import * as middlewares from './middlewares';
+import { requireLogin } from 'routes/middlewares';
 import { badRequestHandler } from 'utils/errorHandler';
 
 const router = Router();
 
-router.param('eventId', middlewares.requestParamHandler);
 router.get('/', validators.getEvents, badRequestHandler, controllers.getEvents);
 router.get('/coordinate', controllers.convertPlaceToCoordinate);
+router.param('eventId', middlewares.requestParamHandler);
 router.get('/:eventId', controllers.getEvent);
 router.get('/:eventId/tickets', controllers.getEventTickets);
+router.patch(
+  '/:eventId/ticket/:ticketId',
+  requireLogin,
+  controllers.checkAttendance,
+);
 
 export default router;
