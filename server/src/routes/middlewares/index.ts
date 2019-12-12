f8a8a1285034ef48cc807901db5375c1a4b8c9c1
문종현 <dobest27@gmail.com>
2019-12-12 21:47:03 +0900

    feat: limitTypeSingleFile middleware

--- server/src/routes/middlewares/index.ts
+++ server/src/routes/middlewares/index.ts
@@ -1,4 +1,5 @@
 export { default as limitSizeSingleFile } from './limitSizeSingleFile';
+export { default as limitTypeSingleFile } from './limitTypeSingleFile';
 export { default as requireSingleFile } from './requireSingleFile';
 export { default as requireLogin } from './requireLogin';
 export { default as singleFileUpload } from './singleFileUpload';
