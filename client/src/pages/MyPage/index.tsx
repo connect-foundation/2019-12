80e41e42f93398559f7b6937a52dccca9d677360
doong-jo <sdong001@gmail.com>
2019-12-12 14:25:40 +0900

    feat: add MyPage template

--- client/src/pages/MyPage/index.tsx
+++ client/src/pages/MyPage/index.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import { EventsState } from 'types/States';
 import MyPageTemplate from './template';
-import { LNB } from 'components';
+import { LNB, CardGrid } from 'components';
 
 function MyPage(): React.ReactElement {
+  const [templateStep, setTemplateStep] = useState(1);
+  const [boughtEvents, setBoughtEvents] = useState([]);
+
   return (
     <MyPageTemplate
+      step={templateStep}
       lnbTab={
         <LNB
-          items={['내 티켓', '호스트', '주최한 이벤트', '프로필', '로그아웃']}
+          items={['내 티켓', '주최한 이벤트', '로그아웃']}
+          onTabClicked={tabIndex => {
+            setTemplateStep(tabIndex);
+          }}
         />
       }
+      ticketsProps={{
+        title: '구매 완료한 티켓',
+        cardGrid: <></>,
+      }}
+      createdEventsProps={{}}
     />
   );
 }
--- client/src/pages/MyPage/template/index.tsx
+++ client/src/pages/MyPage/template/index.tsx
@@ -2,16 +2,36 @@ import React from 'react';
 
 import * as S from './style';
 import BasedTemplate from 'pages/BasedTemplate/templates';
+import TicektsTemplate, { Props as TicketsProps } from './Tickets';
+import CreatedEventsTemplate, {
+  Props as CreatedEventsProps,
+} from './CreatedEvents';
 
 interface Props {
   lnbTab: React.ReactElement;
+  step: number;
+  ticketsProps: TicketsProps;
+  createdEventsProps: CreatedEventsProps;
 }
 
-function MyPageTemplate({ lnbTab }: Props): React.ReactElement {
+const MY_TICKETS_STEP = 1;
+const CREATED_EVENTS_STEP = 2;
+
+function MyPageTemplate({
+  lnbTab,
+  step,
+  ticketsProps,
+  createdEventsProps,
+}: Props): React.ReactElement {
   return (
     <BasedTemplate hasHeaderLine>
-      <S.Container></S.Container>
       {lnbTab}
+      <S.Container>
+        {step == MY_TICKETS_STEP && <TicektsTemplate {...ticketsProps} />}
+        {step == CREATED_EVENTS_STEP && (
+          <CreatedEventsTemplate {...createdEventsProps} />
+        )}
+      </S.Container>
     </BasedTemplate>
   );
 }
