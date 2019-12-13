896d7643d9b7b298c9ebfab5747fa11f1ecc1d52
Yongho Lee <21400564@handong.edu>
2019-12-13 09:52:27 +0900

    리뷰받을 components 추가

---
+++ client/src/components/molecules/SearchMap/index.tsx
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import * as S from './style';
+import { Location } from 'types/Data';
+import { Input, KakaoMap } from 'components';
+
+const { REACT_APP_KAKAO_REST_API_KEY } = process.env;
+
+function SearchMap(): React.ReactElement {
+  const [visible, setVisible] = useState<boolean>(true);
+  const [keyword, setKeyword] = useState<string>('');
+  const [results, setResults] = useState([]);
+  const [location, setLocation] = useState<Location>({
+    latitude: 37.4921311495846,
+    longitude: 127.029771111346,
+  });
+
+  useEffect(() => {
+    if (keyword) search(keyword);
+    else setResults([]);
+  }, [keyword]);
+
+  const search = async (query: string) => {
+    const { data } = await axios.get(
+      `https://dapi.kakao.com/v2/local/search/keyword.json?query=${query}&size=4`,
+      {
+        headers: {
+          Authorization: `KakaoAK ${REACT_APP_KAKAO_REST_API_KEY}`,
+        },
+      },
+    );
+    setResults(data.documents);
+  };
+
+  const handleKeywordChange = async (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setKeyword(e.target.value);
+    setVisible(true);
+  };
+
+  const handleClickResult = (
+    latitude: number,
+    longitude: number,
+    roadAddressName: string,
+  ) => {
+    setVisible(false);
+    setKeyword(roadAddressName);
+    setLocation({ latitude, longitude });
+  };
+
+  return (
+    <S.Container>
+      <S.SearchContainer>
+        <Input
+          inputName="searchMap"
+          value={keyword}
+          onChange={handleKeywordChange}
+          placeholder="서울 서초구 강남대로 327"
+        />
+        <S.DropDown visible={visible}>
+          {results.map((result, index) => {
+            const {
+              x: longitude,
+              y: latitude,
+              road_address_name: roadAddressName,
+              place_name: placeName,
+            } = result;
+            return (
+              <S.DropDownItem
+                key={index}
+                onClick={() =>
+                  handleClickResult(latitude, longitude, roadAddressName)
+                }
+              >
+                <S.PlaceName>{placeName}</S.PlaceName>
+                <S.RoadAddressName>{roadAddressName}</S.RoadAddressName>
+              </S.DropDownItem>
+            );
+          })}
+        </S.DropDown>
+      </S.SearchContainer>
+      <S.KakaoMapWrapper>
+        <KakaoMap {...location} />
+      </S.KakaoMapWrapper>
+    </S.Container>
+  );
+}
+
+export default SearchMap;
