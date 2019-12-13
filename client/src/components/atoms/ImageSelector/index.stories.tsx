b516548f48c7e12c02c3806176f55259296b7cc5
문종현 <dobest27@gmail.com>
2019-12-13 14:18:02 +0900

    feat: add ImageSelector OnChange to storybook

--- client/src/components/atoms/ImageSelector/index.stories.tsx
+++ client/src/components/atoms/ImageSelector/index.stories.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 import ImageSelector from '.';
+import { action } from '@storybook/addon-actions';
 
 export default {
   title: 'Atoms / ImageSelector',
 };
 
 export const index: React.FC = () => <ImageSelector />;
+
+export const onChange: React.FC = () => (
+  <ImageSelector onChange={action('onChange')} />
+);
