4ce6ba25c0a308944a9cc8ea7e7d0e0b0a84c0bb
doong-jo <sdong001@gmail.com>
2019-12-13 10:07:26 +0900

    refactor: refact EventDate useage

--- client/src/components/atoms/EventDate/index.tsx
+++ client/src/components/atoms/EventDate/index.tsx
@@ -2,14 +2,26 @@ import React from 'react';
 
 import * as S from './style';
 import { calculateStringOfDateRange } from 'utils/dateCalculator';
+import { getKoreanDateString } from '../../../utils/dateCalculator/index';
 
 interface EventDateProps {
-  startAt: string;
-  endAt: string;
+  startAt?: string;
+  endAt?: string;
+  children?: string;
 }
 
-function EventDate({ startAt, endAt }: EventDateProps): React.ReactElement {
-  return <S.Wrapper>{calculateStringOfDateRange(startAt, endAt)}</S.Wrapper>;
+function EventDate({
+  startAt,
+  endAt,
+  children,
+}: EventDateProps): React.ReactElement {
+  if (startAt && endAt) {
+    return <S.Wrapper>{calculateStringOfDateRange(startAt, endAt)}</S.Wrapper>;
+  } else if (children) {
+    return <S.Wrapper>{getKoreanDateString(children)}</S.Wrapper>;
+  } else {
+    return <></>;
+  }
 }
 
 export default EventDate;
