50c5e9a64e777ac6416c9ed8aebb16338dc96437
Yongho Lee <21400564@handong.edu>
2019-12-13 10:41:49 +0900

    refactor: use useRef instead of using DOM directly

--- client/src/components/atoms/KakaoMap/index.tsx
+++ client/src/components/atoms/KakaoMap/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 
 import * as S from './style';
-declare const kakao: any;
+declare const kakao: any; // for using kakao map sdk
 
 interface Props {
   latitude: number;
@@ -13,23 +13,26 @@ function KakaoMap({
   longitude,
   height = '28rem',
 }: Props): React.ReactElement {
-  const mapRef = useRef<HTMLInputElement>(null);
+  const kakaoMapRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const options = {
-      center: new kakao.maps.LatLng(latitude, longitude),
-      level: 3,
-    };
-    const kakaoMap = new kakao.maps.Map(mapRef.current, options);
-    const markerPosition = new kakao.maps.LatLng(latitude, longitude);
-    const marker = new kakao.maps.Marker({
-      position: markerPosition,
-    });
+    const kakaoMapElement = kakaoMapRef.current;
+    if (kakaoMapElement) {
+      const options = {
+        center: new kakao.maps.LatLng(latitude, longitude),
+        level: 3,
+      };
+      const kakaoMap = new kakao.maps.Map(kakaoMapElement, options);
+      const markerPosition = new kakao.maps.LatLng(latitude, longitude);
+      const marker = new kakao.maps.Marker({
+        position: markerPosition,
+      });
 
-    marker.setMap(kakaoMap);
+      marker.setMap(kakaoMap);
+    }
   }, [latitude, longitude]);
 
-  return <S.Container ref={mapRef} height={height} />;
+  return <S.Container ref={kakaoMapRef} height={height} />;
 }
 
 export default KakaoMap;
