54402734964813cfe33f364f50d18e6d0171db80
doong-jo <sdong001@gmail.com>
2019-12-17 02:41:32 +0900

    refactor: refact add actions and context

---
+++ client/src/pages/MyPage/actions.ts
@@ -0,0 +1,101 @@
+import { Dispatch } from 'react';
+import { FetchProps } from 'hooks/useApiRequest';
+import { BoughtTicketEvent, CreatedEvent } from 'types/Data';
+import { MyPageAction } from 'types/Actions';
+import { MyPageState } from 'types/States';
+
+export const FETCH_EVENTS = 'FETCH_EVENTS';
+export const FETCH_CREATED_EVENTS = 'FETCH_CREATED_EVENTS';
+
+export interface UseAction {
+  successFetchBoughtTicket: (
+    boughtTicketResponse: FetchProps<BoughtTicketEvent[]>,
+  ) => void;
+  failureFetchBoughtTicket: (
+    boughtTicketResponse: FetchProps<BoughtTicketEvent[]>,
+  ) => void;
+  successFetchCreatedEvents: (
+    createdEventsResponse: FetchProps<CreatedEvent[]>,
+  ) => void;
+  failureFetchCreatedEvents: (
+    createdEventsResponse: FetchProps<CreatedEvent[]>,
+  ) => void;
+}
+
+export function internalActions(
+  { events, eventsOrder, createdEvents, createdEventsOrder }: MyPageState,
+  draftState: MyPageState,
+) {
+  return {
+    [FETCH_EVENTS]: Object.assign(
+      { events: draftState.events, eventsOrder: draftState.eventsOrder },
+      {
+        createdEvents,
+        createdEventsOrder,
+      },
+    ),
+    [FETCH_CREATED_EVENTS]: Object.assign(
+      { events, eventsOrder },
+      {
+        createdEvents: draftState.createdEvents,
+        createdEventsOrder: draftState.createdEventsOrder,
+      },
+    ),
+  };
+}
+
+export default function useAction(dispatch: Dispatch<MyPageAction>) {
+  return {
+    successFetchBoughtTicket: (
+      boughtTicketResponse: FetchProps<BoughtTicketEvent[]>,
+    ) => {
+      const { data } = boughtTicketResponse;
+      console.log('data', data);
+
+      if (data) {
+        const events = new Map<number, BoughtTicketEvent>();
+        const eventsOrder = data.map((event: BoughtTicketEvent) => {
+          events.set(event.id, event);
+          return event.id;
+        });
+        dispatch({
+          type: FETCH_EVENTS,
+          value: {
+            events,
+            eventsOrder,
+          },
+        });
+      }
+    },
+    failureFetchBoughtTicket: (
+      boughtTicketResponse: FetchProps<BoughtTicketEvent[]>,
+    ) => {
+      alert(`요청에 실패했습니다. ${boughtTicketResponse.status}`);
+    },
+    successFetchCreatedEvents: (
+      createdEventsResponse: FetchProps<CreatedEvent[]>,
+    ) => {
+      const { data } = createdEventsResponse;
+
+      if (data) {
+        const createdEvents = new Map<number, CreatedEvent>();
+        const createdEventsOrder = data.map((event: CreatedEvent) => {
+          createdEvents.set(event.id, event);
+          return event.id;
+        });
+        dispatch({
+          type: FETCH_CREATED_EVENTS,
+          value: {
+            createdEvents,
+            createdEventsOrder,
+          },
+        });
+      }
+    },
+    failureFetchCreatedEvents: (
+      createdEventsResponse: FetchProps<CreatedEvent[]>,
+    ) => {
+      alert(`요청에 실패했습니다. ${createdEventsResponse.status}`);
+    },
+  };
+}
--- client/src/pages/MyPage/reducer.ts
+++ client/src/pages/MyPage/reducer.ts
@@ -1,54 +1,30 @@
 import { BoughtTicketEvent, CreatedEvent } from 'types/Data';
-
-interface State {
-  events: Map<number, BoughtTicketEvent>;
-  eventsOrder: number[];
-  createdEvents: Map<number, CreatedEvent>;
-  createdEventsOrder: number[];
-}
-
-interface Action {
-  type: string;
-  value: State;
-}
+import { internalActions, FETCH_EVENTS, FETCH_CREATED_EVENTS } from './actions';
+import { MyPageState } from 'types/States';
+import { MyPageAction } from 'types/Actions';
 
 export interface Reducer {
-  (state: State, action: Action): State;
+  (state: MyPageState, action: MyPageAction): MyPageState;
 }
 
 export const defaultState = {
   events: new Map<number, BoughtTicketEvent>(),
   eventsOrder: [],
-  createdEvents: new Map<number, BoughtTicketEvent>(),
+  createdEvents: new Map<number, CreatedEvent>(),
   createdEventsOrder: [],
 };
 
-export const FETCH_EVENTS = 'FETCH_EVENTS';
-export const FETCH_CREATED_EVENTS = 'FETCH_CREATED_EVENTS';
-export const FETCH_ERROR = 'FETCH_ERROR';
-
 export default function reducer(
-  { events, eventsOrder, createdEvents, createdEventsOrder }: State,
-  action: Action,
-) {
+  state: MyPageState,
+  action: MyPageAction,
+): MyPageState {
   const { type, value } = action;
 
   switch (type) {
     case FETCH_EVENTS:
-      return {
-        events: value.events,
-        eventsOrder: value.eventsOrder,
-        createdEvents,
-        createdEventsOrder,
-      };
-
     case FETCH_CREATED_EVENTS:
-      return {
-        events,
-        eventsOrder,
-        createdEvents: value.createdEvents,
-        createdEventsOrder: value.createdEventsOrder,
-      };
+      console.log('fetched!!');
+      return internalActions(state, value)[type];
 
     default:
       throw new Error(`unexpected action.type: ${type}`);
