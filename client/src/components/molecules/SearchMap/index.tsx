73ec49f3cca980b27ead0291e8e65cd72147c99b
Yongho Lee <21400564@handong.edu>
2019-12-16 13:48:47 +0900

    feat: edit SearchMap

--- client/src/components/molecules/SearchMap/index.tsx
+++ client/src/components/molecules/SearchMap/index.tsx
@@ -2,16 +2,41 @@ import React, { useState, useEffect } from 'react';
 import { searchAddressByKeyword } from 'apis';
 import * as S from './style';
 import { Input, KakaoMap } from 'components';
+import DropDown, { Item as DropDownItem } from '../DropDown';
 
 interface Location {
   latitude: number;
   longitude: number;
 }
+interface SearchResult {
+  road_address_name: string;
+  place_name: string;
+  x: string;
+  y: string;
+}
+
+export const convertResultsToItems = (items: SearchResult[]): DropDownItem[] =>
+  items.map(item => {
+    const {
+      x: longitude,
+      y: latitude,
+      road_address_name: title,
+      place_name: desc,
+    } = item;
+    return {
+      title,
+      desc,
+      value: {
+        latitude,
+        longitude,
+      },
+    };
+  });
 
 function SearchMap(): React.ReactElement {
   const [visible, setVisible] = useState<boolean>(true);
   const [keyword, setKeyword] = useState<string>('');
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState<DropDownItem[]>([]);
   const [location, setLocation] = useState<Location>({
     latitude: 37.4921311495846,
     longitude: 127.029771111346,
@@ -19,12 +44,16 @@ function SearchMap(): React.ReactElement {
 
   useEffect(() => {
     if (keyword) search(keyword);
-    else setResults([]);
+    else {
+      setResults([]);
+      setVisible(false);
+    }
   }, [keyword]);
 
   const search = async (query: string) => {
     const { data } = await searchAddressByKeyword(query, 4);
-    setResults(data.documents);
+    const dropDownItems = convertResultsToItems(data.documents);
+    setResults(dropDownItems);
   };
 
   const handleKeywordChange = async (
@@ -34,14 +63,16 @@ function SearchMap(): React.ReactElement {
     setVisible(true);
   };
 
-  const handleClickResult = (
-    latitude: number,
-    longitude: number,
-    roadAddressName: string,
-  ) => {
+  const handleClickResult = ({
+    title,
+    value,
+  }: {
+    title: string;
+    value: any;
+  }) => {
     setVisible(false);
-    setKeyword(roadAddressName);
-    setLocation({ latitude, longitude });
+    setKeyword(title);
+    setLocation(value);
   };
 
   return (
@@ -53,27 +84,11 @@ function SearchMap(): React.ReactElement {
           onChange={handleKeywordChange}
           placeholder="서울 서초구 강남대로 327"
         />
-        <S.DropDown visible={visible}>
-          {results.map((result, index) => {
-            const {
-              x: longitude,
-              y: latitude,
-              road_address_name: roadAddressName,
-              place_name: placeName,
-            } = result;
-            return (
-              <S.DropDownItem
-                key={index}
-                onClick={() =>
-                  handleClickResult(latitude, longitude, roadAddressName)
-                }
-              >
-                <S.PlaceName>{placeName}</S.PlaceName>
-                <S.RoadAddressName>{roadAddressName}</S.RoadAddressName>
-              </S.DropDownItem>
-            );
-          })}
-        </S.DropDown>
+        <DropDown
+          visible={visible}
+          items={results}
+          handleOnClick={handleClickResult}
+        />
       </S.SearchContainer>
       <S.KakaoMapWrapper>
         <KakaoMap {...location} />
--- client/src/components/molecules/SearchMap/style.tsx
+++ client/src/components/molecules/SearchMap/style.tsx
@@ -1,40 +1,9 @@
 import styled from 'styled-components';
-import { theme, palette } from 'styled-tools';
 
 export const Container = styled.div``;
 
 export const SearchContainer = styled.div`
   position: relative;
-`;
-interface DropDownProps {
-  visible: boolean;
-}
-export const DropDown = styled.ul<DropDownProps>`
-  visibility: ${props => (props.visible ? 'visible' : 'hidden')};
-  position: absolute;
-  background-color: white;
-  padding: 0;
-  z-index: 999;
-  width: 100%;
-  border-bottom-left-radius: 0.5rem;
-  border-bottom-right-radius: 0.5rem;
-  box-shadow: 0 0.4rem 0.2rem -0.2rem ${palette('grayscale', 3)};
-`;
-export const DropDownItem = styled.li`
-  padding: 1.5rem 1rem;
-  list-style: none;
-  &:hover {
-    background-color: ${palette('grayscale', 6)};
-    transition: background-color 0.5s;
-  }
-`;
-export const PlaceName = styled.div`
-  ${theme('fontStyle.body2')}
-  color: ${palette('grayscale', 2)};
-`;
-export const RoadAddressName = styled.div`
-  ${theme('fontStyle.caption')}
-  color: ${palette('grayscale', 3)};
 `;
 export const KakaoMapWrapper = styled.div`
   margin-top: 1rem;
