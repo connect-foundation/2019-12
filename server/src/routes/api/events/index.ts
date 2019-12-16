3a49a283dbb735e3394250f9e7c173bd7272f24a
문종현 <dobest27@gmail.com>
2019-12-16 14:28:32 +0900

    feat: add param validation

--- server/src/routes/api/events/index.ts
+++ server/src/routes/api/events/index.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 import * as controllers from './controllers';
 import * as validators from './validators';
 import * as middlewares from './middlewares';
-import { requireLogin } from 'routes/middlewares';
+import { requireLogin, paramsValidator } from 'routes/middlewares';
 import { badRequestHandler } from 'utils/errorHandler';
 
 const router = Router();
@@ -11,15 +11,22 @@ const router = Router();
 router.param('eventId', middlewares.requestParamHandler);
 router.get('/', validators.getEvents, badRequestHandler, controllers.getEvents);
 router.get('/coordinate', controllers.convertPlaceToCoordinate);
-router.get('/:eventId', controllers.getEvent);
-router.get('/:eventId/tickets', controllers.getEventTickets);
+router.get('/:eventId', paramsValidator('eventId'), controllers.getEvent);
+router.get(
+  '/:eventId/tickets',
+  paramsValidator('eventId'),
+  controllers.getEventTickets,
+);
 router.patch(
   '/:eventId/ticket/:ticketId',
+  paramsValidator('eventId'),
+  paramsValidator('ticketId'),
   requireLogin,
   controllers.checkAttendance,
 );
 router.get(
   '/:eventId/users',
+  paramsValidator('eventId'),
   requireLogin,
   validators.checkAttendance,
   controllers.getAttendants,
