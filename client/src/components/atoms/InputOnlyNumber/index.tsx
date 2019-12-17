c9256c938de3772015534ca2e073aefffc54bd3b
Yongho Lee <21400564@handong.edu>
2019-12-17 20:00:03 +0900

    feat: add prefix for InputOnlyNumber

--- client/src/components/atoms/InputOnlyNumber/index.tsx
+++ client/src/components/atoms/InputOnlyNumber/index.tsx
@@ -2,19 +2,21 @@ import React, { useState } from 'react';
 import Input, { Props as InputProps } from 'components/atoms/Input';
 import numberDecorator from 'utils/numberDecorator';
 
-export function handleNumber(value: string) {
-  const onlyNumber = value.replace(/[^(\d)]/g, '').slice(0, 7);
+export function handleNumber(value: string, prefix?: '₩' | '$') {
+  const onlyNumber = +value.replace(/[^(\d)]/g, '').slice(0, 7);
+  const formattedNumber = numberDecorator({
+    mount: onlyNumber,
+    currency: prefix,
+    separated: true,
+  });
   return {
-    formattedNumber: numberDecorator({
-      mount: +onlyNumber,
-      separated: true,
-    }),
-    onlyNumber,
+    formattedNumber,
+    onlyNumber: onlyNumber.toString(),
   };
 }
 
 interface InputOnlyNumberProps extends InputProps {
-  prefix?: string;
+  prefix?: '₩' | '$';
   handleOnChange?: (value: string) => void;
 }
 function InputOnlyNumber({
@@ -23,9 +25,13 @@ function InputOnlyNumber({
   handleOnChange,
   ...props
 }: InputOnlyNumberProps): React.ReactElement {
-  const [number, setNumber] = useState<string>('');
+  const initialNumber = prefix ? `${prefix} 0` : '0';
+  const [number, setNumber] = useState<string>(initialNumber);
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { formattedNumber, onlyNumber } = handleNumber(e.target.value);
+    const { formattedNumber, onlyNumber } = handleNumber(
+      e.target.value,
+      prefix,
+    );
     if (onlyNumber && handleOnChange) handleOnChange(onlyNumber);
     if (formattedNumber) setNumber(formattedNumber);
   };
