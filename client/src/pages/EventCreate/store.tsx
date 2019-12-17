9e06161e0e5e6e543c1732a8f7fcca528b04f5f9
Yongho Lee <21400564@handong.edu>
2019-12-18 01:02:53 +0900

    feat: edit structure of State

--- client/src/pages/EventCreate/store.tsx
+++ client/src/pages/EventCreate/store.tsx
@@ -1,9 +1,6 @@
 import React, { useEffect, createContext, useReducer, Dispatch } from 'react';
-import { useHistory } from 'react-router-dom';
-import { BAD_REQUEST, FORBIDDEN, OK } from 'http-status';
-
 import { ActionParams } from 'types/Actions';
-import { EventCreateFormState } from 'types/States';
+import { EventFormState, TicketFormState } from 'types/States';
 import { UseStateReducer } from 'types/CustomHooks';
 import { createEvent } from 'apis';
 
@@ -16,26 +13,78 @@ export function useStateReducer<T>(state: T, action: ActionParams<T>): T {
   };
 }
 
-const defaultState: EventCreateFormState = {
+const validateStates = (
+  eventFormStates: EventFormState,
+  ticketFormStates: TicketFormState,
+) =>
+  Object.values(eventFormStates).every(state => state.valid) &&
+  Object.values(ticketFormStates).every(state => state.valid);
+
+const createFormData = (
+  eventFormStates: EventFormState,
+  ticketFormStates: TicketFormState,
+) => {
+  const form: any = {};
+  const ticket: any = {};
+  for (const [key, state] of Object.entries(eventFormStates)) {
+    if (typeof state.value !== 'object') form[key] = state.value;
+    else {
+      for (const [key, value] of Object.entries(state.value)) {
+        form[key] = value;
+      }
+    }
+  }
+  for (const [key, state] of Object.entries(ticketFormStates)) {
+    if (typeof state.value !== 'object') ticket[key] = state.value;
+    else {
+      for (const [key, value] of Object.entries(state.value)) {
+        ticket[key] = value;
+      }
+    }
+  }
+  console.log({ ...form, ticket });
+  // isPublic: true,
+  // title: '이벤트의 제목',
+  // startAt: '2200-12-15 10:00:00',
+  // endAt: '2201-05-01 13:00:00',
+  // place: '패스트파이브 강남 4호점',
+  // address: '서울시 강남구',
+  // placeDesc: '주차 불가',
+  // desc: '설명',
+  // latitude: 37.5662952,
+  // longitude: 126.9779451,
+  // 'ticket[name]': '티켓 이름',
+  // 'ticket[desc]': '티켓 설명',
+  // 'ticket[quantity]': 30,
+  // 'ticket[isPublicLeftCnt]': false,
+  // 'ticket[maxCntPerPerson]': 10,
+  // 'ticket[price]': 10000,
+  // 'ticket[salesStartAt]': '2200-12-15 10:00:00',
+  // 'ticket[salesEndAt]': '2200-12-17 13:00:00',
+  // 'ticket[refundEndAt]': '2200-12-20 13:00:00',
+  // console.log(formData);
+};
+
+const EventFormDefaultState: EventFormState = {
   isPublic: {
     valid: true,
     value: false,
   },
-  eventTitle: {
+  title: {
     valid: false,
     value: '',
   },
-  eventDate: {
+  date: {
     valid: false,
     value: {
       startAt: '',
     },
   },
-  eventPlace: {
+  place: {
     valid: false,
     value: '',
   },
-  eventAddress: {
+  address: {
     valid: false,
     value: {
       address: '',
@@ -43,107 +92,112 @@ const defaultState: EventCreateFormState = {
       longitude: 0,
     },
   },
-  eventPlaceDesc: {
+  placeDesc: {
     valid: false,
     value: '',
   },
-  eventMainImg: {
-    valid: false,
-    value: {},
-  },
-  eventDesc: {
+  mainImg: {
     valid: false,
     value: '',
   },
-  ticketName: {
+  desc: {
     valid: false,
     value: '',
   },
-  ticketDesc: {
+};
+const TicketFormDefaultState: TicketFormState = {
+  name: {
     valid: false,
     value: '',
   },
-  ticketPrice: {
+  desc: {
     valid: false,
     value: '',
   },
-  ticketQuantity: {
+  price: {
     valid: false,
     value: '',
   },
-  ticketIsPublicLeftCnt: {
+  quantity: {
+    valid: false,
+    value: '',
+  },
+  isPublicLeftCnt: {
     valid: false,
     value: false,
   },
-  ticketMaxCntPerPerson: {
+  maxCntPerPerson: {
     valid: false,
     value: '',
   },
-  ticketSalesDate: {
+  salesDate: {
     valid: false,
     value: {
-      startAt: '',
+      salesStartAt: '',
+      salesEndAt: '',
     },
   },
-  ticketRefundDate: {
+  refundDate: {
     valid: false,
     value: {
-      startAt: '',
+      refundEndAt: '',
     },
   },
-  submit: false,
 };
-
-const validateStates = (states: EventCreateFormState) =>
-  Object.values(states).every((state, index, arr) =>
-    index === arr.length - 1 ? true : state.valid,
-  );
-
-const createFormData = (states: EventCreateFormState) => {
-  const formData = new FormData();
-  for (const [key, state] of Object.entries(states)) {
-    formData.append(key, state.value);
-  }
-  console.log(formData);
-  return formData;
-};
-
-export const EventCreateState = createContext<EventCreateFormState>(
-  defaultState,
+export const EventState = createContext<EventFormState>(EventFormDefaultState);
+export const EventAction = createContext<
+  Dispatch<ActionParams<EventFormState>>
+>(() => {});
+export const TicketState = createContext<TicketFormState>(
+  TicketFormDefaultState,
 );
-export const EventCreateAction = createContext<
-  Dispatch<ActionParams<EventCreateFormState>>
+export const TicketAction = createContext<
+  Dispatch<ActionParams<TicketFormState>>
 >(() => {});
 
 function StoreProvider({ children }: { children: React.ReactElement }) {
-  const history = useHistory();
-  const [states, dispatcher] = useReducer<
-    UseStateReducer<EventCreateFormState>
-  >(useStateReducer, defaultState);
-  const formValid = validateStates(states);
+  const [eventFormStates, eventFormDispatcher] = useReducer<
+    UseStateReducer<EventFormState>
+  >(useStateReducer, EventFormDefaultState);
+  const [ticketFormStates, ticketFormDispatcher] = useReducer<
+    UseStateReducer<TicketFormState>
+  >(useStateReducer, TicketFormDefaultState);
+  // const formValid = validateStates(states);
 
-  const { submit } = states;
+  // const { submit } = states;
 
   useEffect(() => {
     console.log('상태 변경!');
-    console.log(states);
-  }, [states]);
+    // console.log('eventFormStates', eventFormStates);
+    // console.log('ticketFormStates', ticketFormStates);
+    console.log(
+      '유효성 -> ',
+      validateStates(eventFormStates, ticketFormStates),
+    );
+    createFormData(eventFormStates, ticketFormStates);
+  }, [eventFormStates, ticketFormStates]);
 
-  useEffect(() => {
-    console.log('formValid', formValid);
-    if (!formValid) {
-      dispatcher({ type: 'submit', value: false });
-      return alert('입력값을 확인해주세요.');
-    }
-    // const createdFormData = createFormData(states);
-  }, [formValid, submit]);
+  // useEffect(() => {
+  //   const createdFormData = createFormData(states);
+  //   // console.log('formValid', formValid);
+  //   // if (!formValid) {
+  //   //   dispatcher({ type: 'submit', value: false });
+  //   //   return alert('입력값을 확인해주세요.');
+  //   // }
+
+  //   console.dir(createdFormData);
+  // }, [submit]);
 
   return (
-    <EventCreateState.Provider value={states}>
-      <EventCreateAction.Provider value={dispatcher}>
-        {children}
-      </EventCreateAction.Provider>
-    </EventCreateState.Provider>
+    <EventState.Provider value={eventFormStates}>
+      <EventAction.Provider value={eventFormDispatcher}>
+        <TicketState.Provider value={ticketFormStates}>
+          <TicketAction.Provider value={ticketFormDispatcher}>
+            {children}
+          </TicketAction.Provider>
+        </TicketState.Provider>
+      </EventAction.Provider>
+    </EventState.Provider>
   );
 }
 
