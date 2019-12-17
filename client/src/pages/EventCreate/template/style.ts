97a1b9c34cdc2bb5ab89eab341710a4f17606099
Yongho Lee <21400564@handong.edu>
2019-12-17 16:06:10 +0900

    feat: edit style of CreateForm

--- client/src/pages/EventCreate/template/style.ts
+++ client/src/pages/EventCreate/template/style.ts
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
 
 export const EventCreateHeader = styled.section``;
-export const EventForm = styled.section``;
+export const EventForm = styled.section`
+  padding: 3rem;
+
+  @media screen and (min-width: 64rem) {
+    padding: 0rem;
+  }
+`;
 export const TicketForm = styled.section``;
 export const CreateBtnWrapper = styled.div`
   display: flex;
