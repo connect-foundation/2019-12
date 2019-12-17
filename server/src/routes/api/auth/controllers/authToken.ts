1ed529b3e48a854ffb942c10279df83406158b2b
FullOfOrange <foorgdev@gmail.com>
2019-12-17 16:05:19 +0900

    feat: token now give userdata

--- server/src/routes/api/auth/controllers/authToken.ts
+++ server/src/routes/api/auth/controllers/authToken.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { verifyJWT } from 'utils/jwt';
+import { getUserById } from 'services/users';
 
 export const authToken = async (
   req: Request,
@@ -9,13 +10,21 @@ export const authToken = async (
   const token = req.cookies.UID;
   if (!token) return res.sendStatus(401);
   try {
-    const { exist, id, googleId, email } = await verifyJWT(token);
-    res.send({
-      exist,
+    const { exist, id } = await verifyJWT(token);
+    if (!exist) return res.sendStatus(401);
+    const result = await getUserById(id);
+    if (!result) return res.status(400).send({ message: 'Cannot get data' });
+    const { firstName, lastName, phoneNumber, email, googleId } = result;
+    const responseData = {
       id,
-      googleId,
+      firstName,
+      lastName,
+      phoneNumber,
       email,
-    });
+      googleId,
+      message: 'Success',
+    };
+    res.send(responseData);
   } catch (err) {
     next(err);
   }
