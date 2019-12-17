57dabb9016f9f328d0e447b09c7819455240841e
문종현 <dobest27@gmail.com>
2019-12-17 15:21:01 +0900

    test: add space between case

--- server/test/utils/validateSignUpForms.spec.ts
+++ server/test/utils/validateSignUpForms.spec.ts
@@ -48,27 +48,26 @@ describe('utils - validatePhoneNumber()', () => {
 describe('utils - validateName()', () => {
   it('이름이 한글이 아닐경우', () => {
     const data = 'aoei';
-
     expect(validateName(data)).toBe(false);
   });
+
   it('이름이 다섯글자가 넘어갈 경우', () => {
     const data = '안녕하세욤ㅇ';
-
     expect(validateName(data)).toBe(false);
   });
+
   it('Type이 기본값일 때, 이름이 입력되지 않을 경우', () => {
     const data = '';
-
     expect(validateName(data)).toBe(true);
   });
+
   it('Type이 True일 때, 이름이 입력되지 않을 경우', () => {
     const data = '';
-
     expect(validateName(data, true)).toBe(false);
   });
+
   it('이름이 맞을 때', () => {
     const data = '안녕하';
-
     expect(validateName(data)).toBe(true);
   });
 });
