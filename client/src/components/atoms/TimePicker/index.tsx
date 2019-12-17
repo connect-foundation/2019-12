1719f9b5426086b573460175e06ee9b33650799c
Yongho Lee <21400564@handong.edu>
2019-12-17 13:48:10 +0900

    feat: add key and initial value for TImePicker

--- client/src/components/atoms/TimePicker/index.tsx
+++ client/src/components/atoms/TimePicker/index.tsx
@@ -9,11 +9,15 @@ const renderOptions = () =>
   Array.from(Array(24).keys()).map(time => {
     const prefix = time < 12 ? '오전' : '오후';
     const displayTime = `${prefix} ${time % 12}:00`;
-    return <option value={`${time}:00`}>{displayTime}</option>;
+    return (
+      <option key={time} value={`${time}:00`}>
+        {displayTime}
+      </option>
+    );
   });
 
 function TimePicker({ onChange }: Props): React.ReactElement {
-  const [time, setTime] = useState();
+  const [time, setTime] = useState<string>('00:00');
   const handleOnChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setTime(e.target.value);
   };
