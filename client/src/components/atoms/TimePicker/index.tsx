00118c73f74132ac997a5235240f3f713cad69cd
Yongho Lee <21400564@handong.edu>
2019-12-18 05:15:34 +0900

    fix: edit calculating for exact hour

--- client/src/components/atoms/TimePicker/index.tsx
+++ client/src/components/atoms/TimePicker/index.tsx
@@ -9,9 +9,10 @@ const renderOptions = () =>
   Array.from(Array(24).keys()).map(time => {
     const prefix = time < 12 ? '오전' : '오후';
     const formattedHour = time % 12 >= 10 ? time % 12 : `0${time % 12}`;
+    const exactHour = time >= 10 ? time : `0${time}`;
     const displayTime = `${prefix} ${formattedHour}:00`;
     return (
-      <option key={time} value={`${formattedHour}:00`}>
+      <option key={time} value={`${exactHour}:00`}>
         {displayTime}
       </option>
     );
