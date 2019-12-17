a2df1c6735cb6e4f919438491c3fa323f44e1ea1
Yongho Lee <21400564@handong.edu>
2019-12-17 09:18:10 +0900

    feat: add EventCreate Template

---
+++ client/src/pages/EventCreate/template/style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const EventCreateHeader = styled.section``;
+export const EventForm = styled.section``;
+export const TicketForm = styled.section``;
+export const CreateBtnWrapper = styled.div`
+  display: flex;
+  margin-bottom: 3rem;
+`;
+export const CreateEventFormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
