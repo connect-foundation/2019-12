9ab9bd55628eb767b1cc41fc9069fcc9a9da5ab3
doong-jo <sdong001@gmail.com>
2019-12-18 22:19:05 +0900

    feat: add disable prop in Ticket

--- client/src/components/organisms/Ticket/index.tsx
+++ client/src/components/organisms/Ticket/index.tsx
@@ -5,10 +5,11 @@ import { TicketType } from 'types/Data';
 import { IconLabel, Price } from 'components';
 import { calculateDiffDaysOfDateRange } from 'utils/dateCalculator';
 
-import { FaUsers, FaCheck, FaRegCalendarAlt } from 'react-icons/fa';
+import { FaTicketAlt, FaCheck, FaRegCalendarAlt } from 'react-icons/fa';
 
 interface Prop extends TicketType {
   count?: number;
+  doneEvent?: boolean;
 }
 
 function Ticket({
@@ -20,31 +21,39 @@ function Ticket({
   salesStartAt,
   salesEndAt,
   leftCnt,
+  quantity,
+  doneEvent,
 }: Prop): React.ReactElement {
+  const remainCnt = quantity - leftCnt;
+  const remainDays = calculateDiffDaysOfDateRange(
+    Date().toString(),
+    salesEndAt,
+  );
   return (
     <>
       <S.TicketLabel>티켓</S.TicketLabel>
       <S.TicketContentContainer>
-        <S.TicketContentWrapContainer>
+        <S.TicketContentWrapContainer disabled={!!doneEvent}>
           <S.TicketPriceWrapper>
             <Price separated>{price}</Price>
           </S.TicketPriceWrapper>
           <S.TicketName>{`${name} ${count ? `* ${count}` : ''}`}</S.TicketName>
           <S.TicketDesc>{desc}</S.TicketDesc>
           <IconLabel
+            icon={<FaTicketAlt size={'1.5rem'} />}
+            labelContent={`${remainCnt}개 남음`}
+          />
+          <IconLabel
             icon={<FaCheck size={'1.5rem'} />}
             labelContent={`1인당 ${maxCntPerPerson}개 구입 가능`}
           />
           <IconLabel
             icon={<FaRegCalendarAlt size={'1.5rem'} />}
-            labelContent={`${calculateDiffDaysOfDateRange(
-              salesStartAt,
-              salesEndAt,
-            )}일 후에 판매마감`}
-          />
-          <IconLabel
-            icon={<FaUsers size={'1.5rem'} />}
-            labelContent={`${leftCnt}개 판매`}
+            labelContent={
+              remainDays <= 0
+                ? '판매기간이 종료되었습니다.'
+                : `${remainDays}일 후에 판매마감`
+            }
           />
         </S.TicketContentWrapContainer>
       </S.TicketContentContainer>
