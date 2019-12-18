24414b213c15d18799f27cbe27219f009ac3fce2
FullOfOrange <foorgdev@gmail.com>
2019-12-18 15:11:30 +0900

    fix: refactor useApiRequest fix if server down

--- client/src/hooks/useApiRequest.ts
+++ client/src/hooks/useApiRequest.ts
@@ -43,7 +43,7 @@ export async function fetchData<T>(
       dispatch({ type: SUCCESS, data, status });
     }
   } catch (err) {
-    return dispatch({ type: FAILURE, err, status: err.response.status });
+    return dispatch({ type: FAILURE, err });
   }
 }
 
