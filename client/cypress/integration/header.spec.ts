20e0b2bbfb523689f7aa04eb79b2c9094b62e6da
doong-jo <sdong001@gmail.com>
2019-12-18 10:35:30 +0900

    test: refact header integration

--- client/cypress/integration/header.spec.ts
+++ client/cypress/integration/header.spec.ts
@@ -2,20 +2,23 @@
 
 context('헤더', () => {
   beforeEach(() => {
-    cy.visit('/');
+    cy.server();
   });
   it('이벤트 주최하기 클릭 시 이벤트 주최 페이지로 이동한다', () => {
+    cy.visit('/');
     cy.setCookie('UID', Cypress.env('auth_token'));
     cy.get('[data-testid=header-create]').click();
     cy.location('pathname').should('eq', '/event/create');
   });
 
   it('로그인 버튼 클릭 시 로그인 페이지로 이동된다.', () => {
+    cy.visit('/');
     cy.get('[data-testid=header-account]').click();
     cy.location('pathname').should('eq', '/login');
   });
 
   it('로고 클릭 시 메인 페이지로 이동한다.', () => {
+    cy.visit('/');
     cy.get('[data-testid=header-home]').click();
     cy.location('pathname').should('eq', '/');
   });
