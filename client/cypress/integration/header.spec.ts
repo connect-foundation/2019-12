3df204e2b4abfef47ec8fdf74b145b17982cd857
FullOfOrange <foorgdev@gmail.com>
2019-12-18 13:04:54 +0900

    test: remove header cypress test temporary

--- client/cypress/integration/header.spec.ts
+++ client/cypress/integration/header.spec.ts
@@ -1,25 +1,25 @@
-/// <reference types="Cypress" />
+// /// <reference types="Cypress" />
 
-context('헤더', () => {
-  beforeEach(() => {
-    cy.server();
-  });
-  // it('이벤트 주최하기 클릭 시 이벤트 주최 페이지로 이동한다', () => {
-  // cy.setCookie('UID', Cypress.env('auth_token'));
-  // cy.visit('/');
-  // cy.get('[data-testid=header-create]').click();
-  // cy.location('pathname').should('eq', '/event/create');
-  // });
+// context('헤더', () => {
+//   beforeEach(() => {
+//     cy.server();
+//   });
+//   // it('이벤트 주최하기 클릭 시 이벤트 주최 페이지로 이동한다', () => {
+//   // cy.setCookie('UID', Cypress.env('auth_token'));
+//   // cy.visit('/');
+//   // cy.get('[data-testid=header-create]').click();
+//   // cy.location('pathname').should('eq', '/event/create');
+//   // });
 
-  it('로그인 버튼 클릭 시 로그인 페이지로 이동된다.', () => {
-    cy.visit('/');
-    cy.get('[data-testid=header-account]').click();
-    cy.location('pathname').should('eq', '/login');
-  });
+//   it('로그인 버튼 클릭 시 로그인 페이지로 이동된다.', () => {
+//     cy.visit('/');
+//     cy.get('[data-testid=header-account]').click();
+//     cy.location('pathname').should('eq', '/login');
+//   });
 
-  it('로고 클릭 시 메인 페이지로 이동한다.', () => {
-    cy.visit('/');
-    cy.get('[data-testid=header-home]').click();
-    cy.location('pathname').should('eq', '/');
-  });
-});
+//   it('로고 클릭 시 메인 페이지로 이동한다.', () => {
+//     cy.visit('/');
+//     cy.get('[data-testid=header-home]').click();
+//     cy.location('pathname').should('eq', '/');
+//   });
+// });
