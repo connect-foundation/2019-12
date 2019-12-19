021cc96c6e4cafcf7f9562432db8907d421decb4
doong-jo <sdong001@gmail.com>
2019-12-19 13:08:16 +0900

    feat: add internal sever error string

--- client/src/commons/constants/string.ts
+++ client/src/commons/constants/string.ts
@@ -5,6 +5,8 @@ export const LOGIN_SOCIAL = '소셜 계정을 사용해서 로그인';
 export const BEFORE_LOGIN = '가입 혹은 로그인';
 export const FOOTER_INFO =
   '대표 이메일 boostcamp@festa.io\n북어스(BookUs) | 대표 부캠이 | 서울특별시 서울구 서울동 서울로 123-1234 | 통신판매업 12345678 | 대표전화 123-1234-1234 (문의는 이메일 바랍니다)';
+export const INTERNAL_SERVER_ERROR =
+  '서버 요청에 실패했습니다. 잠시 후에 다시 시도해주세요';
 
 export const SIGNUP_EMAIL = '이메일';
 export const SIGNUP_LAST_NAME = '성';
@@ -131,7 +133,6 @@ export const BOUGHT_TICKET_EVENT_TITLE_CAPTION = '현재 구매한 티켓 목록
 export const HISTORY_METHOD_PUSH = 'PUSH';
 export const HISTORY_METHOD_REPLACE = 'REPLACE';
 
-
 export const REFUND_TICKET_SUCCESS = '환불이 완료되었습니다.';
 export const REFUND_TICKET_FAILURE = '환불이 실패했습니다.';
 export const NOT_FOUND_BOUGHT_TICKET = '아직 구매한 티켓이 없네요..😅';
@@ -164,4 +165,3 @@ export const FORM_NAME: any = {
     refundEndAt: '티켓 환불 마감 날짜',
   },
 };
-
