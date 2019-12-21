62f5211b929db3a590c83375cb71b3ebab5a50a9
doong-jo <sdong001@gmail.com>
2019-12-21 10:35:24 +0900

    test: add cy wait for re render

--- client/cypress/integration/mypage.spec.ts
+++ client/cypress/integration/mypage.spec.ts
@@ -10,6 +10,10 @@ context('마이페이지', () => {
   it('정상적인 접속이 가능하다.', () => {});
 
   describe('탭', () => {
+    beforeEach(() => {
+      cy.wait(5000);
+    });
+
     it('탭의 "내 티켓"를 클릭하면 탭이 focus(underline) 된다.', () => {
       cy.get('[data-testid=lnb-item]')
         .eq(0)
@@ -67,6 +71,10 @@ context('마이페이지', () => {
   });
 
   describe('(구매한 티켓이 있을 때) 내 티켓', () => {
+    beforeEach(() => {
+      cy.wait(5000);
+    });
+
     it('구매한 티켓들이 표시된다.', () => {
       cy.get('[data-testid=main-card]').within(items => {
         expect(items).to.have.length.above(0);
@@ -80,7 +88,7 @@ context('마이페이지', () => {
       cy.get('[data-testid=ticket-box]');
     });
 
-    it('(환불 마감기간 후라면) 티켓의 환불하기를 클릭하면 alert가 발생한다.', () => {
+    it('(환불 마감기간 후라면) 티켓의 기를 클릭하면 alert가 발생한다.', () => {
       cy.get('[data-testid=main-card]')
         .eq(1)
         .click();
@@ -95,8 +103,15 @@ context('마이페이지', () => {
     });
 
     it('(환불 마감기간 전이라면) 티켓의 환불하기를 클릭하면 alert가 발생하고 내 티켓 페이지로 리다이렉션 된다.', () => {
+      // cy.route({
+      //   method: 'DELETE',
+      //   url: '/api/users/ticket',
+      //   status: 204,
+      //   response: {
+      //     ticketId: data.id,
+      //   },
+      // });
       cy.get('[data-testid=main-card]').click();
-
       const alertStub = cy.stub();
       cy.on('window:alert', alertStub);
       cy.get('[data-testid=refund-btn]')
@@ -104,7 +119,6 @@ context('마이페이지', () => {
         .then(() => {
           expect(alertStub).to.be.calledOnce;
         });
-
       cy.location('pathname').should('eq', 'my/tickets');
     });
   });
@@ -115,6 +129,7 @@ context('마이페이지', () => {
 
   describe('(주최한 이벤트가 있을 때) 주최한 이벤트', () => {
     beforeEach(() => {
+      cy.wait(5000);
       cy.get('[data-testid=lnb-item]')
         .eq(1)
         .click();
